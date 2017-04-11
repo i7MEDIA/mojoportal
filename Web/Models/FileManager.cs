4a240eed23e35c872b8f5fb6f513dc1df0ecd323
Elijah Fowler <j.elijah.fowler@gmail.com>
2017-04-11 15:04:23 -0500

    Better implementation of the subfolder fix

--- Web/Models/FileManager.cs
+++ Web/Models/FileManager.cs
@@ -14,7 +14,6 @@ namespace mojoPortal.Web.Models
 		public string InputId { get; set; }
 		public string CKEditorFuncNumber { get; set; }
 		public object QueryString { get; set; }
-		public object FileBaseUrl { get; set; }
 
 		public string Upload { get; set; }
 		public string Rename { get; set; }
