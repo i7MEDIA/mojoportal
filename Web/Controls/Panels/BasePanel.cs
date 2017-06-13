843fb63697aa33d52413ae0383147f260f3ae448
Elijah Fowler <j.elijah.fowler@gmail.com>
2017-06-13 16:52:22 -0500

    Formatted & added ability to autohide panel if child controls are hidden

--- Web/Controls/Panels/BasePanel.cs
+++ Web/Controls/Panels/BasePanel.cs
@@ -20,216 +20,256 @@ using mojoPortal.Web.Framework;
 
 namespace mojoPortal.Web.UI
 {
-    /// <summary>
-    /// primarily a base class for other panel controls
-    /// by sub classing we can configure properties on different sub classes differently from theme.skin file
-    /// this technique allows a nice way to use additional markup or less markup as needed
-    /// to support different versions of Artisteer skins, jqueryui skin, and makes it possible to use html 5 structural elements like article
-    /// and is intended to help us with our mobile strategy by making the markup rendering more flexible
-    /// </summary>
-    public class BasePanel : Panel
-    {
-        private string element = "div";
+	/// <summary>
+	/// primarily a base class for other panel controls
+	/// by sub classing we can configure properties on different sub classes differently from theme.skin file
+	/// this technique allows a nice way to use additional markup or less markup as needed
+	/// to support different versions of Artisteer skins, jqueryui skin, and makes it possible to use html 5 structural elements like article
+	/// and is intended to help us with our mobile strategy by making the markup rendering more flexible
+	/// </summary>
+	public class BasePanel : Panel
+	{
+		private string element = "div";
 
-        public string Element
-        {
-            get { return element; }
-            set { element = value; }
-        }
+		public string Element
+		{
+			get { return element; }
+			set { element = value; }
+		}
 
-        private string extraCssClasses = string.Empty;
+		private string extraCssClasses = string.Empty;
 
-        public string ExtraCssClasses
-        {
-            get { return extraCssClasses; }
-            set { extraCssClasses = value; }
-        }
+		public string ExtraCssClasses
+		{
+			get { return extraCssClasses; }
+			set { extraCssClasses = value; }
+		}
 
-        private bool renderContentsOnly = false;
+		private bool renderContentsOnly = false;
 
-        public bool RenderContentsOnly
-        {
-            get { return renderContentsOnly; }
-            set { renderContentsOnly = value; }
-        }
+		public bool RenderContentsOnly
+		{
+			get { return renderContentsOnly; }
+			set { renderContentsOnly = value; }
+		}
 
-        private string literalExtraTopContent = string.Empty;
+		private string literalExtraTopContent = string.Empty;
 
-        public string LiteralExtraTopContent
-        {
-            get { return literalExtraTopContent; }
-            set { literalExtraTopContent = value; }
-        }
+		public string LiteralExtraTopContent
+		{
+			get { return literalExtraTopContent; }
+			set { literalExtraTopContent = value; }
+		}
 
-        private string literalExtraBottomContent = string.Empty;
+		private string literalExtraBottomContent = string.Empty;
 
-        public string LiteralExtraBottomContent
-        {
-            get { return literalExtraBottomContent; }
-            set { literalExtraBottomContent = value; }
-        }
+		public string LiteralExtraBottomContent
+		{
+			get { return literalExtraBottomContent; }
+			set { literalExtraBottomContent = value; }
+		}
 
-        private bool detectSideColumn = false;
+		private bool detectSideColumn = false;
 
-        public bool DetectSideColumn
-        {
-            get { return detectSideColumn; }
-            set { detectSideColumn = value; }
-        }
+		public bool DetectSideColumn
+		{
+			get { return detectSideColumn; }
+			set { detectSideColumn = value; }
+		}
 
-        private string columnId = UIHelper.CenterColumnId;
+		private string columnId = UIHelper.CenterColumnId;
 
-        private string sideColumnxtraCssClasses = string.Empty;
+		private string sideColumnxtraCssClasses = string.Empty;
 
-        public string SideColumnxtraCssClasses
-        {
-            get { return sideColumnxtraCssClasses; }
-            set { sideColumnxtraCssClasses = value; }
-        }
+		public string SideColumnxtraCssClasses
+		{
+			get { return sideColumnxtraCssClasses; }
+			set { sideColumnxtraCssClasses = value; }
+		}
 
-        private string sideColumnLiteralExtraTopContent = string.Empty;
+		private string sideColumnLiteralExtraTopContent = string.Empty;
 
-        public string SideColumnLiteralExtraTopContent
-        {
-            get { return sideColumnLiteralExtraTopContent; }
-            set { sideColumnLiteralExtraTopContent = value; }
-        }
+		public string SideColumnLiteralExtraTopContent
+		{
+			get { return sideColumnLiteralExtraTopContent; }
+			set { sideColumnLiteralExtraTopContent = value; }
+		}
 
-        private string sideColumnLiteralExtraBottomContent = string.Empty;
+		private string sideColumnLiteralExtraBottomContent = string.Empty;
 
-        public string SideColumnLiteralExtraBottomContent
-        {
-            get { return sideColumnLiteralExtraBottomContent; }
-            set { sideColumnLiteralExtraBottomContent = value; }
-        }
+		public string SideColumnLiteralExtraBottomContent
+		{
+			get { return sideColumnLiteralExtraBottomContent; }
+			set { sideColumnLiteralExtraBottomContent = value; }
+		}
 
-        private bool renderId = true;
+		private bool renderId = true;
 
-        public bool RenderId
-        {
-            get { return renderId; }
-            set { renderId = value; }
-        }
+		public bool RenderId
+		{
+			get { return renderId; }
+			set { renderId = value; }
+		}
 
-        private bool dontRender = false;
+		private bool dontRender = false;
 
-        public bool DontRender
-        {
-            get { return dontRender; }
-            set { dontRender = value; }
-        }
+		public bool DontRender
+		{
+			get { return dontRender; }
+			set { dontRender = value; }
+		}
 
-        protected override void OnPreRender(EventArgs e)
-        {
-            base.OnPreRender(e);
+		private bool autohide = false;
 
-            if (detectSideColumn)
-            {
-                columnId = this.GetColumnId();
+		public bool Autohide
+		{
+			get => autohide;
+			set => autohide = value;
+		}
 
-                switch (columnId)
-                {
-                    case UIHelper.LeftColumnId:
-                    case UIHelper.RightColumnId:
+		private int countOfVisibleWebControls = 0;
 
-                        //if (sideColumnxtraCssClasses.Length > 0)
-                        //{
-                            extraCssClasses = sideColumnxtraCssClasses;
-                        //}
+		protected override void OnPreRender(EventArgs e)
+		{
+			if (HttpContext.Current == null)
+			{
+				base.OnPreRender(e);
+				return;
+			}
 
-                        //if (sideColumnLiteralExtraTopContent.Length > 0)
-                        //{
-                            literalExtraTopContent = sideColumnLiteralExtraTopContent;
-                        //}
+			countOfVisibleWebControls = GetCountVisibleChildWebControls();
+			Visible = !(autohide && countOfVisibleWebControls == 0);
 
-                        //if (sideColumnLiteralExtraBottomContent.Length > 0)
-                        //{
-                            literalExtraBottomContent = sideColumnLiteralExtraBottomContent;
-                        //}
+			if (detectSideColumn)
+			{
+				columnId = this.GetColumnId();
 
-                        break;
+				switch (columnId)
+				{
+					case UIHelper.LeftColumnId:
+					case UIHelper.RightColumnId:
+						extraCssClasses = sideColumnxtraCssClasses;
+						literalExtraTopContent = sideColumnLiteralExtraTopContent;
+						literalExtraBottomContent = sideColumnLiteralExtraBottomContent;
 
-                    case UIHelper.CenterColumnId:
-                    default:
-                        // nothing to do here
+						break;
 
-                        break;
+					case UIHelper.CenterColumnId:
+					default:
+						// nothing to do here
 
-                }
-            }
+						break;
+				}
 
-            
-            if (extraCssClasses.Length > 0)
-            {
-                if (CssClass.Length > 0)
-                {
-                    //CssClass += " " + extraCssClasses;
-                    if (!CssClass.Contains(extraCssClasses))
-                    {
-                        CssClass = extraCssClasses + " " + CssClass;
-                    }
-                }
-                else
-                {
-                    CssClass = extraCssClasses;
-                }
-            }
-            
-        }
+				base.OnPreRender(e);
+			}
 
-        protected override void Render(HtmlTextWriter writer)
-        {
-            if (HttpContext.Current == null)
-            {
-                writer.Write("[" + this.ID + "]");
-                return;
-            }
+			
+			if (extraCssClasses.Length > 0)
+			{
+				if (CssClass.Length > 0)
+				{
+					if (!CssClass.Contains(extraCssClasses))
+					{
+						CssClass = extraCssClasses + " " + CssClass;
+					}
+				}
+				else
+				{
+					CssClass = extraCssClasses;
+				}
+			}
+		}
 
-            if (dontRender) { return; }
+		protected override void Render(HtmlTextWriter writer)
+		{
+			if (HttpContext.Current == null)
+			{
+				writer.Write("[" + ID + "]");
+				return;
+			}
 
-            if (!renderContentsOnly)
-            {
-                if (renderId)
-                {
-                    //writer.Write("<" + element + " id='" + this.ClientID + "' class='" + CssClass + "'>\n");
-                    writer.Write("<");
-                    writer.Write(element);
-                    writer.Write(" id='" + this.ClientID + "' class='" + CssClass + "'>\n");
-                }
-                else
-                {
-                    //writer.Write("<" + element + " class='" + CssClass + "'>\n");
-                    writer.Write("<");
-                    writer.Write(element);
-                    writer.Write(" class='" + CssClass + "'>\n");
-                    
-                    //writer.WriteBeginTag(element);
-                    //writer.WriteAttribute("class", CssClass);
-                    
-                }
-            }
+			if (dontRender)
+			{
+				return;
+			}
 
-            if (literalExtraTopContent.Length > 0)
-            {
-                writer.Write(literalExtraTopContent);
-            }
+			countOfVisibleWebControls = GetCountVisibleChildWebControls();
 
-            base.RenderContents(writer);
+			if (autohide && countOfVisibleWebControls == 0)
+			{
+				return;
+			}
 
-            if (literalExtraBottomContent.Length > 0)
-            {
-                writer.Write(literalExtraBottomContent);
-            }
+			if (!renderContentsOnly)
+			{
+				if (renderId)
+				{
+					writer.Write("<");
+					writer.Write(element);
+					writer.Write(" id='" + ClientID + "'");
 
-            if (!renderContentsOnly)
-            {
-                writer.Write("\n</" + element + ">");
-                //writer.WriteEndTag(element);
-            }
+					if (!string.IsNullOrWhiteSpace(CssClass))
+					{
+						writer.Write(" class='" + CssClass + "'");
+					}
 
+					writer.Write(">\n");
+				}
+				else
+				{
+					writer.Write("<");
+					writer.Write(element);
 
+					if (!string.IsNullOrWhiteSpace(CssClass))
+					{
+						writer.Write(" class='" + CssClass + "'");
+					}
 
-        }
+					writer.Write(">\n");
+				}
+			}
 
-    }
+			if (literalExtraTopContent.Length > 0)
+			{
+				writer.Write(literalExtraTopContent);
+			}
+
+			base.RenderContents(writer);
+
+			if (literalExtraBottomContent.Length > 0)
+			{
+				writer.Write(literalExtraBottomContent);
+			}
+
+			if (!renderContentsOnly)
+			{
+				writer.Write("\n</" + element + ">");
+			}
+		}
+
+		private int GetCountVisibleChildWebControls()
+		{
+			foreach (Control c in Controls)
+			{
+				if ((c is WebControl) && c.Visible)
+				{
+					return 1;
+				}
+
+				if (c is ContentPlaceHolder)
+				{
+					foreach (Control child in c.Controls)
+					{
+						if ((child is WebControl) && child.Visible)
+						{
+							return 1;
+						}
+					}
+				}
+			}
+
+			return 0;
+		}
+	}
 }
