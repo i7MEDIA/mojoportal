cddb0922245c215539536ab6ec35ac91e3a326c1
Elijah Fowler <j.elijah.fowler@gmail.com>
2017-06-22 11:55:10 -0500

    Formatted, added classes to heading and UL via display settings

--- mojoPortal.Features.UI/Blog/Controls/CategoryListControl.ascx.designer.cs
+++ mojoPortal.Features.UI/Blog/Controls/CategoryListControl.ascx.designer.cs
@@ -13,6 +13,15 @@ namespace mojoPortal.Web.BlogUI {
     public partial class BlogCategories {
         
         /// <summary>
+        /// displaySettings control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::mojoPortal.Web.BlogUI.BlogDisplaySettings displaySettings;
+        
+        /// <summary>
         /// litHeadingOpenTag control.
         /// </summary>
         /// <remarks>
@@ -56,14 +65,5 @@ namespace mojoPortal.Web.BlogUI {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::mojoPortal.Web.UI.TagCloudControl cloud;
-        
-        /// <summary>
-        /// displaySettings control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::mojoPortal.Web.BlogUI.BlogDisplaySettings displaySettings;
     }
 }
