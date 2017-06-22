5c5c2ab9c98549a772a694091703e2ab771d2784
Elijah Fowler <j.elijah.fowler@gmail.com>
2017-06-22 11:50:20 -0500

    Formatted, added classes to heading and UL via display settings

--- mojoPortal.Features.UI/Blog/Controls/ArchiveListControl.ascx.cs
+++ mojoPortal.Features.UI/Blog/Controls/ArchiveListControl.ascx.cs
@@ -1,6 +1,6 @@
-﻿// Author:				        Joe Audette
-// Created:			            2009-05-02
-//	Last Modified:              2012-06-08
+﻿// Author:        Joe Audette
+// Created:       2009-05-02
+// Last Modified: 2017-06-20
 // 
 // The use and distribution terms for this software are covered by the 
 // Common Public License 1.0 (http://opensource.org/licenses/cpl.php)  
@@ -10,98 +10,105 @@
 //
 // You must not remove this notice, or any other, from this software.
 
+using mojoPortal.Business;
+using Resources;
 using System;
 using System.Data;
 using System.Web.UI;
-using mojoPortal.Business;
-using Resources;
 
 namespace mojoPortal.Web.BlogUI
 {
-    public partial class BlogArchiveList : UserControl
-    {
-        private int pageId = -1;
-        private int moduleId = -1;
-        private string siteRoot = string.Empty;
+	public partial class BlogArchiveList : UserControl
+	{
+		private int pageId = -1;
+		private int moduleId = -1;
+		private string siteRoot = string.Empty;
 
 
-        public int PageId
-        {
-            get { return pageId; }
-            set { pageId = value; }
-        }
+		public int PageId
+		{
+			get { return pageId; }
+			set { pageId = value; }
+		}
 
-        public int ModuleId
-        {
-            get { return moduleId; }
-            set { moduleId = value; }
-        }
+		public int ModuleId
+		{
+			get { return moduleId; }
+			set { moduleId = value; }
+		}
 
-        public string SiteRoot
-        {
-            get { return siteRoot; }
-            set { siteRoot = value; }
-        }
+		public string SiteRoot
+		{
+			get { return siteRoot; }
+			set { siteRoot = value; }
+		}
 
-        private string headingElement = "h3";
+		private string headingElement = "h3";
 
-        public string HeadingElement
-        {
-            get { return headingElement; }
-            set { headingElement = value; }
-        }
+		public string HeadingElement
+		{
+			get { return headingElement; }
+			set { headingElement = value; }
+		}
 
-        private string overrideHeadingText = string.Empty;
+		private string overrideHeadingText = string.Empty;
 
-        public string OverrideHeadingText
-        {
-            get { return overrideHeadingText; }
-            set { overrideHeadingText = value; }
-        }
+		public string OverrideHeadingText
+		{
+			get { return overrideHeadingText; }
+			set { overrideHeadingText = value; }
+		}
 
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            
-        }
+		protected void Page_Load(object sender, EventArgs e)
+		{
 
-        protected override void OnPreRender(EventArgs e)
-        {
-            if (this.Visible)
-            {
-                litHeadingOpenTag.Text = "<" + headingElement + ">";
-                litHeadingCloseTag.Text = "</" + headingElement + ">";
-                if (overrideHeadingText.Length > 0)
-                {
-                    litHeading.Text = overrideHeadingText;
-                }
-                else
-                {
-                    litHeading.Text = BlogResources.BlogArchivesLabel;
-                }
+		}
 
-                BindList();
-            }
+		protected override void OnPreRender(EventArgs e)
+		{
+			if (Visible)
+			{
+				litHeadingOpenTag.Text = "<";
+				litHeadingOpenTag.Text += headingElement;
+				litHeadingOpenTag.Text += !string.IsNullOrWhiteSpace(displaySettings.CategoryListHeadingClass) ? " class=\"" + displaySettings.CategoryListHeadingClass + "\"" : string.Empty;
+				litHeadingOpenTag.Text += ">";
 
-            
-            base.OnPreRender(e);
+				litHeadingCloseTag.Text = "</" + headingElement + ">";
 
-        }
+				if (overrideHeadingText.Length > 0)
+				{
+					litHeading.Text = overrideHeadingText;
+				}
+				else
+				{
+					litHeading.Text = BlogResources.BlogArchivesLabel;
+				}
 
-        private void BindList()
-        {
-            if (pageId == -1) { return; }
-            if (moduleId == -1) { return; }
+				BindList();
+			}
 
+			base.OnPreRender(e);
+		}
 
-            using (IDataReader reader = Blog.GetBlogMonthArchive(ModuleId))
-            {
-                dlArchive.DataSource = reader;
-                dlArchive.DataBind();
-            }
-            this.Visible = (dlArchive.Items.Count > 0);
+		private void BindList()
+		{
+			if (pageId == -1)
+			{
+				return;
+			}
 
+			if (moduleId == -1)
+			{
+				return;
+			}
 
-        }
+			using (IDataReader reader = Blog.GetBlogMonthArchive(ModuleId))
+			{
+				dlArchive.DataSource = reader;
+				dlArchive.DataBind();
+			}
 
-    }
+			Visible = (dlArchive.Items.Count > 0);
+		}
+	}
 }
