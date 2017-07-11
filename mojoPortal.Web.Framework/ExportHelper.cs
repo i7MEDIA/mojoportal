3987168debcb98311f9042c20a9cda75d0047081
Elijah Fowler <j.elijah.fowler@gmail.com>
2017-07-11 16:22:10 -0500

    Formatted code and added AutoEscapeStringForCsv method

--- mojoPortal.Web.Framework/ExportHelper.cs
+++ mojoPortal.Web.Framework/ExportHelper.cs
@@ -1,6 +1,6 @@
-// Author:					Joe Audette
-// Created:				2007-07-12
-// Last Modified:			2011-01-06
+// Author:        Joe Audette
+// Created:       2007-07-12
+// Last Modified: 2017-07-11
 // 
 //				
 // The use and distribution terms for this software are covered by the 
@@ -14,222 +14,221 @@
 using System;
 using System.Data;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Web;
-using System.Web.UI.WebControls;
 
 namespace mojoPortal.Web.Framework
 {
-   
-    public static class ExportHelper
-    {
+	public static class ExportHelper
+	{
+		public static void ExportDataTableToCsv(
+			HttpContext context,
+			DataTable table,
+			string filename)
+		{
+			if (context == null) { return; }
+			context.Response.ClearHeaders(); // this fixed an issue downloading in IE over ssl
+			context.Response.Clear();
+			context.Response.Buffer = true;
+			context.Response.ContentType = "text/csv";
 
-        public static void ExportDataTableToCsv(
-            HttpContext context, 
-            DataTable table, 
-            string filename)
-	    {
-            if (context == null) { return; }
-            context.Response.ClearHeaders(); // this fixed an issue downloading in IE over ssl
-		    context.Response.Clear();
-		    context.Response.Buffer = true;
-		    context.Response.ContentType = "text/csv";
+			context.Response.AppendHeader("Content-Disposition", "attachment;filename=\"" + filename + "\"");
+			Encoding encoding = new UTF8Encoding();
+			context.Response.ContentEncoding = encoding;
 
-		    context.Response.AppendHeader("Content-Disposition", "attachment;filename=\"" + filename + "\"");
-		    //context.Response.Charset = "";
-            Encoding encoding = new UTF8Encoding();
-            context.Response.ContentEncoding = encoding;
+			StreamWriter sw = new StreamWriter(context.Response.OutputStream);
 
-		    StreamWriter sw = new StreamWriter(context.Response.OutputStream);
-		    
-		    int iColCount = table.Columns.Count;
-		    int i;
-		    for (i = 0; i <= iColCount - 1; i++) {
-			    sw.Write(table.Columns[i]);
-			    if (i < iColCount - 1)
-			    {
-				    sw.Write(",");
-			    }
-		    }
-		    sw.Write(sw.NewLine);
+			int iColCount = table.Columns.Count;
+			int i;
 
-		    i = 0;
+			for (i = 0; i <= iColCount - 1; i++)
+			{
+				sw.Write(table.Columns[i]);
 
-		    foreach (DataRow row in table.Rows) {
+				if (i < iColCount - 1)
+				{
+					sw.Write(",");
+				}
+			}
 
-			    for (i = 0; i <= iColCount - 1; i++) {
-				    if (!Convert.IsDBNull(row[i]))
-				    {
-					    sw.Write("\"");
-                        // see #7 in this article
-                        //http://tools.ietf.org/html/rfc4180
-					    sw.Write(row[i].ToString().CsvEscapeQuotes());
-					    sw.Write("\"");
-				    }
-				    else
-				    {
-					    sw.Write("");
-				    }
-				    if (i < iColCount - 1)
-				    {
-					    sw.Write(",");
-				    }
-			    }
-			    sw.Write(sw.NewLine);
-		    }
-		    sw.Close();
+			sw.Write(sw.NewLine);
 
-		    context.Response.End();
+			i = 0;
 
-	    }
+			foreach (DataRow row in table.Rows)
+			{
+				for (i = 0; i <= iColCount - 1; i++)
+				{
+					if (!Convert.IsDBNull(row[i]))
+					{
+						sw.Write("\"");
+						// see #7 in this article
+						//http://tools.ietf.org/html/rfc4180
+						sw.Write(row[i].ToString().CsvEscapeQuotes());
+						sw.Write("\"");
+					}
+					else
+					{
+						sw.Write("");
+					}
 
+					if (i < iColCount - 1)
+					{
+						sw.Write(",");
+					}
+				}
 
-        
-        public static void ExportDataTableToWord(
-            HttpContext context, 
-            DataTable table,
-            string filename)
-        {
-            if (context == null) { return; }
+				sw.Write(sw.NewLine);
+			}
 
-            StringBuilder wordString = new StringBuilder();
+			sw.Close();
 
-            wordString.Append(@"<html " +
-                    "xmlns:o='urn:schemas-microsoft-com:office:office' " +
-                    "xmlns:w='urn:schemas-microsoft-com:office:word'" +
-                    "xmlns='http://www.w3.org/TR/REC-html40'>" +
-                    "<head><title>Time</title>");
-
-            wordString.Append(@"<!--[if gte mso 9]>" +
-                                     "<xml>" +
-                                     "<w:WordDocument>" +
-                                     "<w:View>Print</w:View>" +
-                                     "<w:Zoom>90</w:Zoom>" +
-                                     "</w:WordDocument>" +
-                                     "</xml>" +
-                                     "<![endif]-->");
-
-            wordString.Append(@"<style>" +
-                                    "<!-- /* Style Definitions */" +
-                                    "@page Section1" +
-                                    "   {size:8.5in 11.0in; " +
-                                    "   margin:1.0in 1.25in 1.0in 1.25in ; " +
-                                    "   mso-header-margin:.5in; " +
-                                    "   mso-footer-margin:.5in; mso-paper-source:0;}" +
-                                    " div.Section1" +
-                                    "   {page:Section1;}" +
-                                    "-->" +
-                                   "</style></head>");
-
-            wordString.Append(@"<body lang=EN-US style='tab-interval:.5in'>" +
-                                    "<div class=Section1>" +
-                                    "<h1>Time and tide wait for none</h1>" +
-                                    "<p style='color:red'><I>" +
-                                    DateTime.Now + "</I></p></div><table border='1'>");
-
-            
-            wordString.Append("<tr>");
-            for (int i = 0; i < table.Columns.Count; i++)
-            {
-                wordString.Append("<td>" + table.Columns[i].ColumnName + "</td>");
-            }
-            wordString.Append("</tr>");
-
-            //Items
-            for (int x = 0; x < table.Rows.Count; x++)
-            {
-                wordString.Append("<tr>");
-                for (int i = 0; i < table.Columns.Count; i++)
-                {
-                    wordString.Append("<td>" + table.Rows[x][i] + "</td>");
-                }
-                wordString.Append("</tr>");
-            }
-
-            wordString.Append(@"</table></body></html>");
-
-            context.Response.ClearHeaders(); // this fixed an issue downloading in IE over ssl
-            context.Response.Clear();
-            context.Response.Buffer = true;
-            context.Response.ContentType = "application/msword";
-
-            context.Response.AppendHeader("Content-Disposition", "attachment;filename=\"" + filename + "\"");
-            //context.Response.Charset = "";
-            Encoding encoding = new UTF8Encoding();
-            context.Response.ContentEncoding = encoding;
-            context.Response.Write(wordString.ToString());
-            
-        }
-
-        //http://www.code.colostate.edu/export-fwp-to-word.aspx
-
-        //public static string DataTable2WordString(System.Data.DataTable pDT)
-        //{
-        //    StringBuilder sbTop = new StringBuilder();
-
-        //    sbTop.Append("<html " + "xmlns:o='urn:schemas-microsoft-com:office:office' " + "xmlns:w='urn:schemas-microsoft-com:office:word'" + "xmlns='http://www.w3.org/TR/REC-html40'>" + "<head><title>Time</title>");
-
-        //    sbTop.Append("<!--[if gte mso 9]>" + "<xml>" + "<w:WordDocument>" + "<w:View>Print</w:View>" + "<w:Zoom>90</w:Zoom>" + "</w:WordDocument>" + "</xml>" + "<![endif]-->");
-
-        //    sbTop.Append("<style>" + "<!-- /* Style Definitions */" + "@page Section1" + "   {size:8.5in 11.0in; " + "   margin:1.0in 1.25in 1.0in 1.25in ; " + "   mso-header-margin:.5in; " + "   mso-footer-margin:.5in; mso-paper-source:0;}" + " div.Section1" + "   {page:Section1;}" + "-->" + "</style></head>");
-
-        //    sbTop.Append(("<body lang=EN-US style='tab-interval:.5in'>" + "<div class=Section1>" + "<h1>" + pDT.TableName + "</h1>" + "<p style='color:red'><I>") + DateTime.Now + "</I></p></div><table border='0'>");
-
-        //    string bottom = "</table></body></html>";
-
-        //    StringBuilder sb = new StringBuilder();
-
-        //    foreach (DataRow row in pDT.Rows)
-        //    {
-        //        sb.Append("<tr>");
-
-        //        foreach (DataColumn col in pDT.Columns)
-        //        {
-        //            if (row[col].ToString() == "Date:")
-        //            {
-        //                sb.Append("</tr><tr><td>&nbsp;</td><td>&nbsp;</td></tr>");
-        //            }
-
-        //            sb.Append("<td>");
-        //            sb.Append(row[col]);
-        //            sb.Append("</td>");
-        //        }
-        //        sb.Append("</tr>");
-        //    }
-
-        //    string SSxml = sbTop.ToString() + sb.ToString() + bottom;
-
-        //    return SSxml;
-
-        //}
+			context.Response.End();
+		}
 
 
-        public static void ExportStringAsFile(
-            HttpContext context,
-            Encoding encoding,
-            string mimeType,
-            string content,
-            string filename)
-        {
-            if (context == null) { return; }
-            context.Response.Clear();
-            context.Response.Buffer = true;
-            context.Response.ContentType = mimeType;
+		public static void ExportDataTableToWord(
+			HttpContext context,
+			DataTable table,
+			string filename)
+		{
+			if (context == null)
+			{
+				return;
+			}
 
-            context.Response.AppendHeader("Content-Disposition", "attachment;filename=\"" + filename + "\"");
-            
-            context.Response.ContentEncoding = encoding;
+			StringBuilder wordString = new StringBuilder();
 
-            StreamWriter sw = new StreamWriter(context.Response.OutputStream);
+			wordString.Append(
+				@"<html " +
+				"xmlns:o='urn:schemas-microsoft-com:office:office' " +
+				"xmlns:w='urn:schemas-microsoft-com:office:word'" +
+				"xmlns='http://www.w3.org/TR/REC-html40'>" +
+				"<head><title>Time</title>"
+			);
 
-            sw.Write(content);
-            
-            sw.Close();
+			wordString.Append(
+				@"<!--[if gte mso 9]>" +
+				"<xml>" +
+				"<w:WordDocument>" +
+				"<w:View>Print</w:View>" +
+				"<w:Zoom>90</w:Zoom>" +
+				"</w:WordDocument>" +
+				"</xml>" +
+				"<![endif]-->"
+			);
 
-            context.Response.End();
+			wordString.Append(
+				@"<style>" +
+				"<!-- /* Style Definitions */ " +
+				"@page Section1 {" +
+				"size: 8.5in 11.0in;" +
+				"margin: 1.0in 1.25in 1.0in 1.25in;" +
+				"mso-header-margin: .5in;" +
+				"mso-footer-margin: .5in;" +
+				"mso-paper-source: 0;" +
+				"} " +
+				"div.Section1 {" +
+				"page: Section1;" +
+				"}" +
+				"-->" +
+				"</style>" +
+				"</head>"
+			);
 
-        }
+			wordString.Append(
+				@"<body lang=EN-US style='tab-interval:.5in'>" +
+				"<div class=Section1>" +
+				"<h1>Time and tide wait for none</h1>" +
+				"<p style='color:red'><I>" +
+				DateTime.Now + "</I></p></div><table border='1'>"
+			);
 
-    }
+			wordString.Append("<tr>");
 
+			for (int i = 0; i < table.Columns.Count; i++)
+			{
+				wordString.Append("<td>" + table.Columns[i].ColumnName + "</td>");
+			}
+
+			wordString.Append("</tr>");
+
+			//Items
+			for (int x = 0; x < table.Rows.Count; x++)
+			{
+				wordString.Append("<tr>");
+
+				for (int i = 0; i < table.Columns.Count; i++)
+				{
+					wordString.Append("<td>" + table.Rows[x][i] + "</td>");
+				}
+
+				wordString.Append("</tr>");
+			}
+
+			wordString.Append(@"</table></body></html>");
+
+			context.Response.ClearHeaders(); // this fixed an issue downloading in IE over ssl
+			context.Response.Clear();
+			context.Response.Buffer = true;
+			context.Response.ContentType = "application/msword";
+
+			context.Response.AppendHeader("Content-Disposition", "attachment;filename=\"" + filename + "\"");
+			Encoding encoding = new UTF8Encoding();
+			context.Response.ContentEncoding = encoding;
+			context.Response.Write(wordString.ToString());
+		}
+
+		public static void ExportStringAsFile(
+			HttpContext context,
+			Encoding encoding,
+			string mimeType,
+			string content,
+			string filename)
+		{
+			if (context == null) { return; }
+			context.Response.Clear();
+			context.Response.Buffer = true;
+			context.Response.ContentType = mimeType;
+
+			context.Response.AppendHeader("Content-Disposition", "attachment;filename=\"" + filename + "\"");
+
+			context.Response.ContentEncoding = encoding;
+
+			StreamWriter sw = new StreamWriter(context.Response.OutputStream);
+
+			sw.Write(content);
+
+			sw.Close();
+
+			context.Response.End();
+		}
+
+		public static string AutoEscapeStringForCsv(string data)
+		{
+			bool wrap = false;
+
+			// If the data has quote, escape the quote in the data
+			if (data.Contains("\""))
+			{
+				data = data.Replace("\"", "\"\"");
+				wrap = true;
+			}
+
+			// If the data contains any non-letter/digit, newline, or tab, set to wrap in quotes
+			if (data.Any(c => !char.IsLetterOrDigit(c)))
+			{
+				wrap = true;
+			}
+
+			// Wrap in double-quotes
+			if (wrap)
+			{
+				data = String.Format("\"{0}\"", data);
+			}
+
+			return data;
+		}
+	}
 }
