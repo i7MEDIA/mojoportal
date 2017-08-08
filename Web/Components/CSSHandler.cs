89fb006d78815993440da34833aef647acab4c78
Elijah Fowler <j.elijah.fowler@gmail.com>
2017-08-08 12:05:47 -0500

    Cleaned up, formatted, and updated CSSHandler
    Cleaned up checks for "http://", "https://", "data:" in urls, and added check for "//" by using RegEx in a DRY manner.
    Added private trimQuotes method to keep things DRY.
    Cleaned up formatting.

--- Web/Components/CSSHandler.cs
+++ Web/Components/CSSHandler.cs
@@ -1,6 +1,6 @@
-﻿//		Author:			
+﻿//		Author:
 //		Created:		2008-10-31
-//		Last Modified:	2014-12-09
+//		Last Modified:	2017-08-08
 // 
 // The use and distribution terms for this software are covered by the 
 // Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
@@ -43,33 +43,42 @@ namespace mojoPortal.Web.UI
 
 		private string skinVersion = string.Empty;
 
-		//private readonly static Regex URL_REGEX =
-		//    new Regex(@"url\((\""|\')?(?<path>(.*))?(\""|\')?\)", RegexOptions.Compiled);
+		//private readonly static Regex URL_REGEX = new Regex(@"url\((\""|\')?(?<path>(.*))?(\""|\')?\)", RegexOptions.Compiled);
 
 		// 2014-12-09 URL_REGEX changed from the above per Todd Hansen to fix multi url on a line problem
-		//https://www.mojoportal.com/Forums/Thread.aspx?pageid=5&t=12924~1#post53334
+		// https://www.mojoportal.com/Forums/Thread.aspx?pageid=5&t=12924~1#post53334
 
 		private readonly static Regex URL_REGEX = new Regex(@"url\((\""|\')?(?<path>(.*?))?(\""|\')?\)", RegexOptions.Compiled);
-		private StringBuilder less = null;
 
+		// We don't want to append "/Data/Sites/[site number]/skins/[skin folder]" to external links
+		// so we check all "url()" links if they start with "http://", "https://", "data:", and "//" to ignore them if they do
+		private readonly static Regex urlPathsToIgnore = new Regex(@"^(https?://|data:|//)");
+
+		// 1 string to keep things DRY
+		private string urlReplacement = "url(\"{0}\")";
+
+		private StringBuilder less = null;
 
 		public void ProcessRequest(HttpContext context)
 		{
 			context.Response.ContentType = "text/css";
 
-			if (context.Request.RequestType == POST) { return; }
+			if (context.Request.RequestType == POST)
+			{
+				return;
+			}
 
-
-			bool isCompressed = DO_GZIP && this.CanGZip(context.Request);
-			//bool isCompressed = false;
+			bool isCompressed = DO_GZIP && CanGZip(context.Request);
 
 			int siteId = SiteUtils.ParseSiteIdFromSkinRequestUrl();
 
 			string skinName = "styleshout-refresh";
+
 			if (context.Request["skin"] != null)
 			{
 				skinName = SiteUtils.SanitizeSkinParam(context.Request["skin"]);
 			}
+
 			string media = "screen";
 
 			if (context.Request["media"] != null)
@@ -81,7 +90,7 @@ namespace mojoPortal.Web.UI
 
 			UTF8Encoding encoding = new UTF8Encoding(false);
 
-			if (!this.WriteFromCache(context, siteId, skinName, isCompressed))
+			if (!WriteFromCache(context, siteId, skinName, isCompressed))
 			{
 				byte[] cssBytes = GetCss(context, siteId, skinName, encoding);
 
@@ -92,7 +101,6 @@ namespace mojoPortal.Web.UI
 						memoryStream)
 					{
 						writer.Write(cssBytes, 0, cssBytes.Length);
-
 					}
 
 					byte[] responseBytes = memoryStream.ToArray();
@@ -103,16 +111,18 @@ namespace mojoPortal.Web.UI
 						lock (this)
 						{
 							string cahceKey = GetCacheKey(siteId, skinName, isCompressed);
+
 							context.Cache.Insert(
-							cahceKey,
-							responseBytes,
-							null,
-							System.Web.Caching.Cache.NoAbsoluteExpiration,
-							CACHE_DURATION);
+								cahceKey,
+								responseBytes,
+								null,
+								System.Web.Caching.Cache.NoAbsoluteExpiration,
+								CACHE_DURATION
+							);
 						}
 					}
 
-					this.WriteBytes(responseBytes, context, isCompressed);
+					WriteBytes(responseBytes, context, isCompressed);
 				}
 			}
 		}
@@ -120,10 +130,15 @@ namespace mojoPortal.Web.UI
 		private bool HasNoCacheCookie()
 		{
 			SiteSettings siteSettings = CacheHelper.GetCurrentSiteSettings();
+
 			if (siteSettings != null)
 			{
 				string cookieName = SiteUtils.GetCssCacheCookieName(siteSettings);
-				if (CookieHelper.CookieExists(cookieName)) { return true; }
+
+				if (CookieHelper.CookieExists(cookieName))
+				{
+					return true;
+				}
 			}
 
 			return false;
@@ -131,28 +146,40 @@ namespace mojoPortal.Web.UI
 
 		private bool ShouldCacheOnServer()
 		{
-			if (HasNoCacheCookie()) { return false; }
+			if (HasNoCacheCookie())
+			{
+				return false;
+			}
 
 			return WebConfigSettings.CacheCssOnServer;
 		}
 
 		private bool ShouldCacheInBrowser()
 		{
-			if (HasNoCacheCookie()) { return false; }
+			if (HasNoCacheCookie())
+			{
+				return false;
+			}
 
 			return WebConfigSettings.CacheCssInBrowser;
 		}
 
 		private void WriteBytes(byte[] bytes, HttpContext context, bool isCompressed)
 		{
-			if (bytes.Length == 0) { return; }
+			if (bytes.Length == 0)
+			{
+				return;
+			}
 
 			HttpResponse response = context.Response;
 
 			response.AppendHeader("Content-Length", bytes.Length.ToInvariantString());
 			response.ContentType = "text/css";
+
 			if (isCompressed)
+			{
 				response.AppendHeader("Content-Encoding", "gzip");
+			}
 
 			bool isIE6 = BrowserHelper.IsIE6();
 
@@ -161,6 +188,7 @@ namespace mojoPortal.Web.UI
 				context.Response.Cache.SetCacheability(HttpCacheability.Public);
 				context.Response.Cache.SetExpires(DateTime.Now.Add(CACHE_DURATION));
 				context.Response.Cache.SetMaxAge(CACHE_DURATION);
+
 				if (!isIE6)
 				{
 					context.Response.Cache.AppendCacheExtension("must-revalidate, proxy-revalidate");
@@ -168,7 +196,7 @@ namespace mojoPortal.Web.UI
 			}
 			else
 			{
-				if ((!ShouldCacheOnServer()) && ((!isIE6)))
+				if (!ShouldCacheOnServer() && !isIE6)
 				{
 					//if both cache settings are off it must be a designer so make it easy
 					context.Response.Cache.SetExpires(new DateTime(1995, 5, 6, 12, 0, 0, DateTimeKind.Utc));
@@ -176,22 +204,24 @@ namespace mojoPortal.Web.UI
 					context.Response.Cache.SetCacheability(HttpCacheability.NoCache);
 					context.Response.Cache.SetRevalidation(HttpCacheRevalidation.AllCaches);
 					context.Response.Cache.AppendCacheExtension("post-check=0,pre-check=0");
-
 				}
 			}
 
 			response.OutputStream.Write(bytes, 0, bytes.Length);
-			//response.Flush();
 		}
 
 		private byte[] GetCss(HttpContext context, int siteId, string skinName, Encoding encoding)
 		{
-
 			string basePath = HttpContext.Current.Server.MapPath(
-				"~/Data/Sites/" + siteId.ToInvariantString()
-				+ "/skins/" + skinName + "/");
+				"~/Data/Sites/" +
+				siteId.ToInvariantString() +
+				"/skins/" +
+				skinName +
+				"/"
+			);
 
 			string siteRoot = string.Empty;
+
 			if (WebConfigSettings.UseFullUrlsForSkins)
 			{
 				siteRoot = WebUtils.GetSiteRoot();
@@ -201,8 +231,14 @@ namespace mojoPortal.Web.UI
 				siteRoot = WebUtils.GetRelativeSiteRoot();
 			}
 
-			string skinImageBasePath = siteRoot + "/Data/Sites/" + siteId.ToInvariantString()
-				+ "/skins/" + skinName + "/";
+			string skinImageBasePath = 
+				siteRoot +
+				"/Data/Sites/" +
+				siteId.ToInvariantString() +
+				"/skins/" +
+				skinName +
+				"/"
+			;
 
 			StringBuilder cssContent = new StringBuilder();
 			bool hasLessFiles = false;
@@ -218,6 +254,7 @@ namespace mojoPortal.Web.UI
 			if (WebConfigSettings.GlobalAddOnStyleFolder.Length > 0)
 			{
 				basePath = HttpContext.Current.Server.MapPath(WebConfigSettings.GlobalAddOnStyleFolder);
+
 				if (File.Exists(basePath + "style.config"))
 				{
 					skinImageBasePath = siteRoot + WebConfigSettings.GlobalAddOnStyleFolder.Replace("~/", "/");
@@ -228,7 +265,7 @@ namespace mojoPortal.Web.UI
 
 			string finalCss;
 
-			if ((hasLessFiles) && (less != null))
+			if (hasLessFiles && (less != null))
 			{
 				finalCss = ProcessLess(less.ToString()) + cssContent.ToString();
 			}
@@ -254,6 +291,7 @@ namespace mojoPortal.Web.UI
 			using (XmlReader reader = new XmlTextReader(new StreamReader(basePath + "style.config")))
 			{
 				reader.MoveToContent();
+
 				while (reader.Read())
 				{
 					if (("file" == reader.Name) && (reader.NodeType != XmlNodeType.EndElement))
@@ -262,60 +300,70 @@ namespace mojoPortal.Web.UI
 						string csswebconfigkey = reader.GetAttribute("csswebconfigkey");
 						string imagebasewebconfigkey = reader.GetAttribute("imagebasewebconfigkey");
 
-						if ((!string.IsNullOrEmpty(csswebconfigkey)) && (csswebconfigkey.Contains("YUI"))) { csswebconfigkey = string.Empty; }
-
-						if ((WebConfigSettings.UseGoogleCDN) && (!string.IsNullOrEmpty(csswebconfigkey)))
+						if (!string.IsNullOrEmpty(csswebconfigkey) && csswebconfigkey.Contains("YUI"))
 						{
-							if (csswebconfigkey.Contains("YUI")) { csswebconfigkey = string.Empty; }
+							csswebconfigkey = string.Empty;
+						}
+
+						if (WebConfigSettings.UseGoogleCDN && !string.IsNullOrEmpty(csswebconfigkey))
+						{
+							if (csswebconfigkey.Contains("YUI"))
+							{
+								csswebconfigkey = string.Empty;
+							}
 						}
 
 						// full virtual path option for things that don't move 
 						string cssVPath = reader.GetAttribute("cssvpath");
 						string imageBaseVPath = reader.GetAttribute("imagebasevpath");
 
-
-
-						if ((!string.IsNullOrEmpty(csswebconfigkey)) && (!string.IsNullOrEmpty(imagebasewebconfigkey)))
+						if (
+							!string.IsNullOrEmpty(csswebconfigkey) &&
+							!string.IsNullOrEmpty(imagebasewebconfigkey)
+						)
 						{
 							if (
-								(ConfigurationManager.AppSettings[csswebconfigkey] != null)
-								&& (ConfigurationManager.AppSettings[imagebasewebconfigkey] != null)
-							 )
+								ConfigurationManager.AppSettings[csswebconfigkey] != null && 
+								ConfigurationManager.AppSettings[imagebasewebconfigkey] != null
+							)
 							{
 
 								string cssFullPath = HttpContext.Current.Server.MapPath(ConfigurationManager.AppSettings[csswebconfigkey]);
 								string imageBasePath = ConfigurationManager.AppSettings[imagebasewebconfigkey];
+
 								if (File.Exists(cssFullPath))
 								{
 									FileInfo file = new FileInfo(cssFullPath);
-									if (file.Extension == ".less") { hasLessFiles = true; }
+
+									if (file.Extension == ".less")
+									{
+										hasLessFiles = true;
+									}
+
 									using (StreamReader sr = file.OpenText())
 									{
 										string fileContent = sr.ReadToEnd();
 
-										//if (file.Extension == ".less")
-										//{
-										//    AddToLess(fileContent);
-										//}
-										//else
-										//{
-
-										string css = URL_REGEX.Replace(fileContent,
-											new MatchEvaluator(delegate (Match m)
-											{
-												string imgPath = m.Groups["path"].Value.TrimStart('\'').TrimEnd('\'').TrimStart('"').TrimEnd('"');
-
-												if (!imgPath.StartsWith("http://") && !imgPath.StartsWith("https://") && !imgPath.Contains("data:"))
+										string css = URL_REGEX.Replace(
+											fileContent,
+											new MatchEvaluator(
+												delegate (Match m)
 												{
-													return "url('" + siteRoot + imageBasePath // First put the prefix for the images
-														+ imgPath // the relative URL as it is in the CSS
-														+ "')";
+													string imgPath = trimQuotes(m.Groups["path"].Value);
+
+													// If paths are external or are a Data URL
+													if (urlPathsToIgnore.Match(imgPath).Success)
+													{
+														return string.Format(urlReplacement, imgPath);
+													}
+													else
+													{
+														// Prefix the image path
+														return string.Format(urlReplacement, siteRoot + imageBasePath + imgPath);
+													}
 												}
-												else
-												{
-													return "url('" + imgPath + "')";
-												}
-											}));
+											)
+										);
 
 										if (file.Extension == ".less")
 										{
@@ -323,10 +371,8 @@ namespace mojoPortal.Web.UI
 										}
 										else
 										{
-
 											cssContent.Append(css);
 										}
-										//}
 									}
 								}
 							}
@@ -334,6 +380,7 @@ namespace mojoPortal.Web.UI
 						else if ((!string.IsNullOrEmpty(cssVPath)) && (!string.IsNullOrEmpty(imageBaseVPath)))
 						{
 							string cssFilePath;
+
 							if (cssVPath.StartsWith("/"))
 							{
 								cssFilePath = HttpContext.Current.Server.MapPath("~" + cssVPath);
@@ -342,6 +389,7 @@ namespace mojoPortal.Web.UI
 							{
 								cssFilePath = HttpContext.Current.Server.MapPath(cssVPath);
 							}
+
 							if (File.Exists(cssFilePath))
 							{
 								FileInfo file = new FileInfo(cssFilePath);
@@ -350,30 +398,25 @@ namespace mojoPortal.Web.UI
 								{
 									string fileContent = sr.ReadToEnd();
 
-									//if (file.Extension == ".less")
-									//{
-									//    AddToLess(fileContent);
-									//}
-									//else
-									//{
-
 									string css = URL_REGEX.Replace(fileContent,
-										new MatchEvaluator(delegate (Match m)
-										{
-											string imgPath = m.Groups["path"].Value.TrimStart('\'').TrimEnd('\'').TrimStart('"').TrimEnd('"');
-
-
-											if (!imgPath.StartsWith("http://") && !imgPath.StartsWith("https://") && !imgPath.Contains("data:"))
+										new MatchEvaluator(
+											delegate (Match m)
 											{
-												return "url('" + siteRoot + imageBaseVPath // First put the prefix for the images
-													+ imgPath // the relative URL as it is in the CSS
-													+ "')";
+												string imgPath = trimQuotes(m.Groups["path"].Value);
+
+												// If paths are external or are a Data URL
+												if (urlPathsToIgnore.Match(imgPath).Success)
+												{
+													return string.Format(urlReplacement, imgPath);
+												}
+												else
+												{
+													// Prefix the image path
+													return string.Format(urlReplacement, siteRoot + imageBaseVPath + imgPath);
+												}
 											}
-											else
-											{
-												return "url('" + imgPath + "')";
-											}
-										}));
+										)
+									);
 
 									if (file.Extension == ".less")
 									{
@@ -383,7 +426,6 @@ namespace mojoPortal.Web.UI
 									{
 										cssContent.Append(css);
 									}
-									//}
 								}
 							}
 						}
@@ -394,71 +436,41 @@ namespace mojoPortal.Web.UI
 							if (File.Exists(basePath + cssFile))
 							{
 								FileInfo file = new FileInfo(basePath + cssFile);
-								if (file.Extension == ".less") { hasLessFiles = true; }
+
+								if (file.Extension == ".less")
+								{
+									hasLessFiles = true;
+								}
+
 								using (StreamReader sr = file.OpenText())
 								{
 									string fileContent = sr.ReadToEnd();
-									//if (file.Extension == ".less")
-									//{
-									//    AddToLess(fileContent);
-									//}
-									//else
-									//{
 
-									//for Artisteer examples like this:
-									//url('/Data/Sites/1/skins/QLawGic_02_mojo/images/nav.png'), -o-linear-gradient(top, #1E2A18 0, #435E36 20%, #25341E 50%, #0E130B 80%, #192414 100%') no-repeat;
-									// move the , - to a new line to solve a regex problem
 									if (cssFile == "style.css")
 									{
-										// string replacement is expensive only do it on the style.css file to fix problems with Artisteer
-										//fileContent = fileContent.Replace(", -", "\n, -").Replace(", linear-gradient", "\n, linear-gradient");
 										fileContent = fileContent.Replace(")", ")\n");
 										fileContent = fileContent.Replace("form.art-search", "div.art-search");
 									}
 
 									string css = URL_REGEX.Replace(fileContent,
-										new MatchEvaluator(delegate (Match m)
-										{
-											string imgPath = m.Groups["path"].Value.TrimStart('\'').TrimEnd('\'').TrimStart('"').TrimEnd('"');
-
-
-
-											//example problem url from artisteer 4 
-											// imgPath is images/object217709083.png'), url('images/header.png
-											// the second url is not getting the skinImageBasePath
-
-											if (!imgPath.StartsWith("http://") && !imgPath.StartsWith("https://") && !imgPath.Contains("data:"))
+										new MatchEvaluator(
+											delegate (Match m)
 											{
-												// background-image: url('/Data/Sites/1/skins/art4-try3/images/object217709083.png'), url('images/header.png');
+												string imgPath = trimQuotes(m.Groups["path"].Value);
 
-												string result = "url('" + skinImageBasePath // First put the prefix for the images
-													+ imgPath // the relative URL as it is in the CSS
-													+ "')";
-
-												// this is kind of hacky, would be better if we could solve it by better regex
-
-												//if (result.Contains(", url('"))
-												//{
-												//    result = result.Replace(", url('", ", url('" + skinImageBasePath);
-												//}
-												//else if (result.Contains(",url('"))
-												//{
-												//    result = result.Replace(",url('", ",url('" + skinImageBasePath);
-												//}
-
-												//if (result.Contains(",  url('"))
-												//{
-												//    result = result.Replace(",  url('", ",  url('" + skinImageBasePath);
-												//}
-
-												return result;
+												// If paths are external or are a Data URL
+												if (urlPathsToIgnore.Match(imgPath).Success)
+												{
+													return string.Format(urlReplacement, imgPath);
+												}
+												else
+												{
+													// Prefix the image path
+													return string.Format(urlReplacement, skinImageBasePath + imgPath);
+												}
 											}
-											else
-											{
-												return "url('" + imgPath + "')";
-											}
-										}));
-
+										)
+									);
 
 									if (file.Extension == ".less")
 									{
@@ -468,8 +480,6 @@ namespace mojoPortal.Web.UI
 									{
 										cssContent.Append(css);
 									}
-									//}
-									//cssContent.Append(fileContent);
 								}
 							}
 						}
@@ -478,9 +488,20 @@ namespace mojoPortal.Web.UI
 			}
 		}
 
+		// Added to keep things DRY
+		private string trimQuotes(string url)
+		{
+			char[] quotes = {'"', '\''};
+
+			return url.Trim(quotes);
+		}
+
 		private void EnsureLessBuilder()
 		{
-			if (less == null) { less = new StringBuilder(); }
+			if (less == null)
+			{
+				less = new StringBuilder();
+			}
 		}
 
 		private void AddToLess(string lessContent)
@@ -502,7 +523,6 @@ namespace mojoPortal.Web.UI
 			config.HandleWebCompression = false;
 			config.Debug = WebConfigSettings.DebugDotLess;
 
-			//string css = Less.Parse(finalCss, config);
 			string css = LessWeb.Parse(finalCss, config);
 
 			if (string.IsNullOrEmpty(css))
@@ -522,7 +542,7 @@ namespace mojoPortal.Web.UI
 			if (null == responseBytes) { return false; }
 			if (responseBytes.Length == 0) { return false; }
 
-			this.WriteBytes(responseBytes, context, isCompressed);
+			WriteBytes(responseBytes, context, isCompressed);
 			return true;
 		}
 
