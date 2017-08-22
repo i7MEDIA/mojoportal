5e137c552290921ec5d31c36068b16f27f2ad818
JosephMdavis <joe@i7media.net>
2017-08-22 11:42:27 -0500

    Blog: Fix PostListLayoutSelector not respecting page skin.

--- mojoPortal.Features.UI/Blog/Controls/PostListLayoutSelector.ascx.designer.cs
+++ mojoPortal.Features.UI/Blog/Controls/PostListLayoutSelector.ascx.designer.cs
@@ -20,5 +20,14 @@ namespace mojoPortal.Web.BlogUI {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.DropDownList ddlLayouts;
+        
+        /// <summary>
+        /// litNoLayouts control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal litNoLayouts;
     }
 }
