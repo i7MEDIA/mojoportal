c16b6c128ff3976f7bc4c014cd5b23925968714e
Elijah Fowler <j.elijah.fowler@gmail.com>
2017-08-23 10:19:54 -0500

    Added Pagination class and ItemsPerPage property to PostListModel

--- mojoPortal.Features.Business/Blog/PostListModel.cs
+++ mojoPortal.Features.Business/Blog/PostListModel.cs
@@ -7,5 +7,11 @@ namespace mojoPortal.Business
 		public string ModuleTitle { get; set; }
 		public string ModulePageUrl { get; set; }
 		public IEnumerable<BlogPostModel> Posts { get; set; }
+		public Pagination Pagination { get; set; }
+	}
+
+	public class Pagination
+	{
+		public int ItemsPerPage { get; set; }
 	}
 }
