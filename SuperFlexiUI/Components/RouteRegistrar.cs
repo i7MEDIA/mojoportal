43de58df9d8cd7b32ee854e8eafee33b009f7a0e
Joe Davis <joe@i7media.net>
2017-10-25 16:55:49 -0500

    SuperFlexi: beginning work on WebApi

---
+++ SuperFlexiUI/Components/RouteRegistrar.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using mojoPortal.Web.Routing;
+using System.Web.Routing;
+using System.Web.Mvc;
+
+
+namespace SuperFlexiUI
+{
+    public class RouteRegistrar : IRegisterRoutes
+    {
+        public void Register(HttpConfiguration config)
+        {
+           // api routes
+
+            config.Routes.MapHttpRoute(
+                name: "GetSflexiData",
+                routeTemplate: "api/sflexi/get/{id}",
+                defaults: new { controller = "SuperFlexi", id = RouteParameter.Optional }
+            );
+
+        }
+
+        public void RegisterRoutes(RouteCollection routes)
+        {
+            //mvc routes
+
+        }
+
+        public void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+
+        }
+
+    }
+}
---
+++ SuperFlexiUI/SuperFlexiController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using log4net;
+using SuperFlexiBusiness;
+using mojoPortal.Business;
+namespace SuperFlexiUI
+{
+
+	public class DataRequest
+	{
+		[HttpBindRequired]
+		public int PageId { get; set; }
+		[HttpBindRequired]
+		public int ModuleId { get; set; }
+		[HttpBindRequired]
+		public int PageNumber { get; set; }
+		[HttpBindRequired]
+		public int PageSize { get; set; }
+		[HttpBindRequired]
+		public int ItemId { get; set; }
+		[HttpBindRequired]
+		public string SortDirection { get; set; }
+		[HttpBindRequired]
+		public string SortField { get; set; }
+	}
+
+	public class SuperFlexiController : ApiController
+	{
+		private static readonly ILog log = LogManager.GetLogger(typeof(SuperFlexiController));
+
+		private Module module = null;
+		private SiteSettings siteSettings = null;
+		private PageSettings currentPage = null;
+		private SiteUser postUser = null;
+		private ModuleConfiguration config = new ModuleConfiguration();
+		// GET api/<controller>
+		public IEnumerable<string> Get()
+		{
+			return new string[] { "value1", "value2" };
+		}
+
+		// GET api/<controller>/5
+		public string Get(int id)
+		{
+			return "value";
+		}
+
+		// POST api/<controller>
+		public void Post([FromBody]DataRequest dataRequest)
+		{
+
+
+		}
+
+		// PUT api/<controller>/5
+		public void Put(int id, [FromBody]string value)
+		{
+		}
+
+		// DELETE api/<controller>/5
+		public void Delete(int id)
+		{
+		}
+	}
+}
