85d61bf35a71ba4692bf47315b075f6c9c791e56
Elijah Fowler <j.elijah.fowler@gmail.com>
2018-08-31 10:21:32 -0500

    Features | Survey: Cleaned up PostgreSQL DBQuestionAnswer file
    Converted sqlCommand from a StringBuilder to a verbatim string, converted sqlParameter array into list.

--- mojoPortal.Features.Data.pgsql/Survey/DBQuestionAnswer.cs
+++ mojoPortal.Features.Data.pgsql/Survey/DBQuestionAnswer.cs
@@ -1,172 +1,195 @@
-﻿/// Author:				
-/// Created:			2008-08-29
-/// Last Modified:		2012-08-13 
-/// 
-/// The use and distribution terms for this software are covered by the 
-/// Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
-/// which can be found in the file CPL.TXT at the root of this distribution.
-/// By using this software in any fashion, you are agreeing to be bound by 
-/// the terms of this license.
-///
-/// You must not remove this notice, or any other, from this software.
+﻿// Created:       2008-08-29
+// Last Modified: 2012-08-13 
+// 
+// The use and distribution terms for this software are covered by the 
+// Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
+// which can be found in the file CPL.TXT at the root of this distribution.
+// By using this software in any fashion, you are agreeing to be bound by 
+// the terms of this license.
+//
+// You must not remove this notice, or any other, from this software.
 
 
-using System;
-using System.Data;
-using System.Text;
 using mojoPortal.Data;
 using Npgsql;
+using NpgsqlTypes;
+using System;
+using System.Collections.Generic;
+using System.Data;
 
 namespace SurveyFeature.Data
 {
-    
-    public static class DBQuestionAnswer
-    {
-        /// <summary>
-        /// Inserts a row in the mp_SurveyQuestionAnswers table. Returns rows affected count.
-        /// </summary>
-        /// <param name="answerGuid"> answerGuid </param>
-        /// <param name="questionGuid"> questionGuid </param>
-        /// <param name="responseGuid"> responseGuid </param>
-        /// <param name="answer"> answer </param>
-        /// <returns>int</returns>
-        public static int Add(
-            Guid answerGuid,
-            Guid questionGuid,
-            Guid responseGuid,
-            string answer)
-        {
-            StringBuilder sqlCommand = new StringBuilder();
-            sqlCommand.Append("INSERT INTO mp_surveyquestionanswers (");
-            sqlCommand.Append("answerguid, ");
-            sqlCommand.Append("questionguid, ");
-            sqlCommand.Append("responseguid, ");
-            sqlCommand.Append("answer, ");
-            sqlCommand.Append("answereddate )");
+	public static class DBQuestionAnswer
+	{
+		/// <summary>
+		///     Inserts a row in the mp_SurveyQuestionAnswers table. Returns rows affected count.
+		/// </summary>
+		/// <param name="answerGuid"> answerGuid </param>
+		/// <param name="questionGuid"> questionGuid </param>
+		/// <param name="responseGuid"> responseGuid </param>
+		/// <param name="answer"> answer </param>
+		/// <returns>int</returns>
+		public static int Add(
+			Guid answerGuid,
+			Guid questionGuid,
+			Guid responseGuid,
+			string answer
+		)
+		{
+			const string sqlCommand = @"
+				INSERT INTO
+					mp_surveyquestionanswers (
+						answerguid,
+						questionguid,
+						responseguid,
+						answer,
+						answereddate
+					)
+					VALUES (
+						:answerguid,
+						:questionguid,
+						:responseguid,
+						:answer,
+						:answereddate
+					);";
 
-            sqlCommand.Append(" VALUES (");
-            sqlCommand.Append(":answerguid, ");
-            sqlCommand.Append(":questionguid, ");
-            sqlCommand.Append(":responseguid, ");
-            sqlCommand.Append(":answer, ");
-            sqlCommand.Append(":answereddate ");
-            sqlCommand.Append(")");
-            sqlCommand.Append(";");
+			var arParams = new List<NpgsqlParameter>
+			{
+				new NpgsqlParameter("answerguid", NpgsqlDbType.Char, 36)
+				{
+					Direction = ParameterDirection.Input,
+					Value = answerGuid.ToString()
+				},
+				new NpgsqlParameter("questionguid", NpgsqlDbType.Char, 36)
+				{
+					Direction = ParameterDirection.Input,
+					Value = questionGuid.ToString()
+				},
+				new NpgsqlParameter("responseguid", NpgsqlDbType.Char, 36)
+				{
+					Direction = ParameterDirection.Input,
+					Value = responseGuid.ToString()
+				},
+				new NpgsqlParameter("answer", NpgsqlDbType.Text)
+				{
+					Direction = ParameterDirection.Input,
+					Value = answer
+				},
+				new NpgsqlParameter("answereddate", NpgsqlDbType.Timestamp)
+				{
+					Direction = ParameterDirection.Input,
+					Value = DateTime.UtcNow
+				}
+			};
 
-            NpgsqlParameter[] arParams = new NpgsqlParameter[5];
+			var rowsAffected = NpgsqlHelper.ExecuteNonQuery(
+				ConnectionString.GetWriteConnectionString(),
+				CommandType.Text,
+				sqlCommand,
+				arParams.ToArray()
+			);
 
-            arParams[0] = new NpgsqlParameter("answerguid", NpgsqlTypes.NpgsqlDbType.Char, 36);
-            arParams[0].Direction = ParameterDirection.Input;
-            arParams[0].Value = answerGuid.ToString();
-
-            arParams[1] = new NpgsqlParameter("questionguid", NpgsqlTypes.NpgsqlDbType.Char, 36);
-            arParams[1].Direction = ParameterDirection.Input;
-            arParams[1].Value = questionGuid.ToString();
-
-            arParams[2] = new NpgsqlParameter("responseguid", NpgsqlTypes.NpgsqlDbType.Char, 36);
-            arParams[2].Direction = ParameterDirection.Input;
-            arParams[2].Value = responseGuid.ToString();
-
-            arParams[3] = new NpgsqlParameter("answer", NpgsqlTypes.NpgsqlDbType.Text);
-            arParams[3].Direction = ParameterDirection.Input;
-            arParams[3].Value = answer;
-
-            arParams[4] = new NpgsqlParameter("answereddate", NpgsqlTypes.NpgsqlDbType.Timestamp);
-            arParams[4].Direction = ParameterDirection.Input;
-            arParams[4].Value = DateTime.UtcNow;
-
-            int rowsAffected = NpgsqlHelper.ExecuteNonQuery(ConnectionString.GetWriteConnectionString(),
-                CommandType.Text,
-                sqlCommand.ToString(),
-                arParams);
-
-            return rowsAffected;
-
-        }
-
-        /// <summary>
-        /// Updates a row in the mp_SurveyQuestionAnswers table. Returns true if row updated.
-        /// </summary>
-        /// <param name="answerGuid"> answerGuid </param>
-        /// <param name="questionGuid"> questionGuid </param>
-        /// <param name="responseGuid"> responseGuid </param>
-        /// <param name="answer"> answer </param>
-        /// <returns>bool</returns>
-        public static bool Update(
-            Guid answerGuid,
-            Guid questionGuid,
-            Guid responseGuid,
-            string answer)
-        {
-            StringBuilder sqlCommand = new StringBuilder();
-            sqlCommand.Append("UPDATE mp_surveyquestionanswers ");
-            sqlCommand.Append("SET  ");
-            sqlCommand.Append("questionguid = :questionguid, ");
-            sqlCommand.Append("responseguid = :responseguid, ");
-            sqlCommand.Append("answer = :answer ");
-
-            sqlCommand.Append("WHERE  ");
-            sqlCommand.Append("answerguid = :answerguid ");
-            sqlCommand.Append(";");
-
-            NpgsqlParameter[] arParams = new NpgsqlParameter[4];
-
-            arParams[0] = new NpgsqlParameter("answerguid", NpgsqlTypes.NpgsqlDbType.Char, 36);
-            arParams[0].Direction = ParameterDirection.Input;
-            arParams[0].Value = answerGuid.ToString();
-
-            arParams[1] = new NpgsqlParameter("questionguid", NpgsqlTypes.NpgsqlDbType.Char, 36);
-            arParams[1].Direction = ParameterDirection.Input;
-            arParams[1].Value = questionGuid.ToString();
-
-            arParams[2] = new NpgsqlParameter("responseguid", NpgsqlTypes.NpgsqlDbType.Char, 36);
-            arParams[2].Direction = ParameterDirection.Input;
-            arParams[2].Value = responseGuid.ToString();
-
-            arParams[3] = new NpgsqlParameter("answer", NpgsqlTypes.NpgsqlDbType.Text);
-            arParams[3].Direction = ParameterDirection.Input;
-            arParams[3].Value = answer;
-
-            int rowsAffected = NpgsqlHelper.ExecuteNonQuery(ConnectionString.GetWriteConnectionString(),
-                CommandType.Text,
-                sqlCommand.ToString(),
-                arParams);
-
-            return (rowsAffected > -1);
-
-        }
-
-        /// <summary>
-        /// Gets an IDataReader with one row from the mp_SurveyQuestionAnswers table.
-        /// </summary>
-        /// <param name="answerGuid"> answerGuid </param>
-        public static IDataReader GetOne(Guid responseGuid, Guid questionGuid)
-        {
-            StringBuilder sqlCommand = new StringBuilder();
-            sqlCommand.Append("SELECT  * ");
-            sqlCommand.Append("FROM	mp_surveyquestionanswers ");
-            sqlCommand.Append("WHERE ");
-            sqlCommand.Append("questionguid = :questionguid ");
-            sqlCommand.Append("AND responseguid = :responseguid; ");
-
-            NpgsqlParameter[] arParams = new NpgsqlParameter[2];
-
-            arParams[0] = new NpgsqlParameter("questionguid", NpgsqlTypes.NpgsqlDbType.Char, 36);
-            arParams[0].Direction = ParameterDirection.Input;
-            arParams[0].Value = questionGuid.ToString();
-
-            arParams[1] = new NpgsqlParameter("responseguid", NpgsqlTypes.NpgsqlDbType.Char, 36);
-            arParams[1].Direction = ParameterDirection.Input;
-            arParams[1].Value = responseGuid.ToString();
-
-            return NpgsqlHelper.ExecuteReader(
-                ConnectionString.GetReadConnectionString(),
-                CommandType.Text,
-                sqlCommand.ToString(),
-                arParams);
-
-        }
+			return rowsAffected;
+		}
 
 
-    }
+		/// <summary>
+		///     Updates a row in the mp_SurveyQuestionAnswers table. Returns true if row updated.
+		/// </summary>
+		/// <param name="answerGuid"> answerGuid </param>
+		/// <param name="questionGuid"> questionGuid </param>
+		/// <param name="responseGuid"> responseGuid </param>
+		/// <param name="answer"> answer </param>
+		/// <returns>bool</returns>
+		public static bool Update(
+			Guid answerGuid,
+			Guid questionGuid,
+			Guid responseGuid,
+			string answer
+		)
+		{
+			const string sqlCommand = @"
+				UPDATE
+					mp_surveyquestionanswers
+				SET 
+					questionguid = :questionguid,
+					responseguid = :responseguid,
+					answer = :answer
+				WHERE
+					answerguid = :answerguid;";
+
+			var arParams = new List<NpgsqlParameter>
+			{
+				new NpgsqlParameter("answerguid", NpgsqlDbType.Char, 36)
+				{
+					Direction = ParameterDirection.Input,
+					Value = answerGuid.ToString()
+				},
+				new NpgsqlParameter("questionguid", NpgsqlDbType.Char, 36)
+				{
+					Direction = ParameterDirection.Input,
+					Value = questionGuid.ToString()
+				},
+				new NpgsqlParameter("responseguid", NpgsqlDbType.Char, 36)
+				{
+					Direction = ParameterDirection.Input,
+					Value = responseGuid.ToString()
+				},
+				new NpgsqlParameter("answer", NpgsqlDbType.Text)
+				{
+					Direction = ParameterDirection.Input,
+					Value = answer
+				}
+			};
+
+			var rowsAffected = NpgsqlHelper.ExecuteNonQuery(
+				ConnectionString.GetWriteConnectionString(),
+				CommandType.Text,
+				sqlCommand,
+				arParams.ToArray()
+			);
+
+			return rowsAffected > -1;
+		}
+
+
+		/// <summary>
+		///     Gets an IDataReader with one row from the mp_SurveyQuestionAnswers table.
+		/// </summary>
+		/// <param name="responseGuid"></param>
+		/// <param name="questionGuid"></param>
+		public static IDataReader GetOne(Guid responseGuid, Guid questionGuid)
+		{
+			const string sqlCommand = @"
+				SELECT
+					*
+				FROM
+					mp_surveyquestionanswers 
+				WHERE
+					questionguid = :questionguid
+				AND
+					responseguid = :responseguid;";
+
+			var arParams = new List<NpgsqlParameter>
+			{
+				new NpgsqlParameter("questionguid", NpgsqlDbType.Char, 36)
+				{
+					Direction = ParameterDirection.Input,
+					Value = questionGuid.ToString()
+				},
+				new NpgsqlParameter("responseguid", NpgsqlDbType.Char, 36)
+				{
+					Direction = ParameterDirection.Input,
+					Value = responseGuid.ToString()
+				}
+			};
+
+			return NpgsqlHelper.ExecuteReader(
+				ConnectionString.GetReadConnectionString(),
+				CommandType.Text,
+				sqlCommand,
+				arParams.ToArray()
+			);
+		}
+	}
 }
