f75f5318e8e84f316a1cb5f9463dc7e2916dd101
Elijah Fowler <j.elijah.fowler@gmail.com>
2018-08-31 19:58:19 -0500

    Core - MySQL: Added method to append "SslMode=none" to connection string
    This is legacy support for the latest ADO update.

--- mojoPortal.Data.MySql/ConnectionString.cs
+++ mojoPortal.Data.MySql/ConnectionString.cs
@@ -1,24 +1,42 @@
-﻿using System;
-using System.Configuration;
+﻿using System.Configuration;
 
 namespace mojoPortal.Data
 {
-    public static class ConnectionString
-    {
-        public static String GetReadConnectionString()
-        {
-            return ConfigurationManager.AppSettings["MySqlConnectionString"];
+	public static class ConnectionString
+	{
+		private const string StringName = "MySqlConnectionString";
+		private const string WriteStringName = "MySqlWriteConnectionString";
 
-        }
 
-        public static String GetWriteConnectionString()
-        {
-            if (ConfigurationManager.AppSettings["MySqlWriteConnectionString"] != null)
-            {
-                return ConfigurationManager.AppSettings["MySqlWriteConnectionString"];
-            }
+		public static string GetReadConnectionString()
+		{
+			return GetString();
+		}
 
-            return ConfigurationManager.AppSettings["MySqlConnectionString"];
-        }
-    }
+
+		public static string GetWriteConnectionString()
+		{
+			return GetString("write") ?? GetString();
+		}
+
+
+		private static string GetString(string type = "get")
+		{
+			switch (type)
+			{
+				default:
+					return ConfigurationManager.AppSettings[StringName].AddSslMode();
+				case "write":
+					return ConfigurationManager.AppSettings[WriteStringName].AddSslMode();
+			}
+		}
+
+
+		private static string AddSslMode(this string cstring)
+		{
+			return cstring.Contains("SslMode")
+				? cstring
+				: cstring.TrimEnd(';') + ";SslMode=none;";
+		}
+	}
 }
