e8e55fcbdb7e636109d89ca88740327a7fe34a75
Elijah Fowler <j.elijah.fowler@gmail.com>
2019-02-21 17:17:57 -0600

    Core: Started work on tagging system
    Cleaned up files Audette created, modified stored procedures to update tag count when tags are created/modified/deleted, created some methods in TagRepository.

--- mojoPortal.Business/Tag/TagRepository.cs
+++ mojoPortal.Business/Tag/TagRepository.cs
@@ -1,11 +1,168 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using mojoPortal.Data;
+
+using System;
+using System.Data;
+
 
 namespace mojoPortal.Business
 {
-    class TagRepository
-    {
-    }
+	public class TagRepository
+	{
+		#region Public Tag Methods
+
+		public Tag SaveTag(Tag tag, out bool result)
+		{
+			if (tag.Guid == Guid.Empty)
+			{
+				tag.Guid = Guid.NewGuid();
+
+				result = DBTag.Create(
+					tag.Guid,
+					tag.SiteGuid,
+					tag.FeatureGuid,
+					tag.ModuleGuid,
+					tag.TagText,
+					tag.CreatedUtc,
+					tag.CreatedBy,
+					tag.VocabularyGuid
+				);
+			}
+			else
+			{
+				result = DBTag.Update(
+					tag.Guid,
+					tag.TagText,
+					tag.ModifiedUtc,
+					tag.ModifiedBy,
+					tag.VocabularyGuid
+				);
+			}
+
+			return tag;
+		}
+
+		public bool DeleteTagByGuid(Guid guid)
+		{
+			return DBTag.Delete(guid);
+		}
+
+		public bool DeleteTagByModuleGuid(Guid moduleGuid)
+		{
+			return DBTag.DeleteByModule(moduleGuid);
+		}
+
+		public bool DeleteTagByFeatureGuid(Guid featureGuid)
+		{
+			return DBTag.DeleteByFeature(featureGuid);
+		}
+
+		public bool DeleteTagBySiteGuid(Guid siteGuid)
+		{
+			return DBTag.DeleteBySite(siteGuid);
+		}
+
+		public Tag GetTagByGuid(Guid tagGuid)
+		{
+			using (IDataReader reader = DBTag.GetOneTag(tagGuid))
+			{
+				return getTagFromIDataReader(reader);
+			}
+		}
+
+		public Tag GetTagByModuleGuid(Guid moduleGuid)
+		{
+			using (IDataReader reader = DBTag.GetByModule(moduleGuid))
+			{
+				return getTagFromIDataReader(reader);
+			}
+		}
+
+		#endregion
+
+
+		#region Public TagItem Methods
+
+		public TagItem SaveTagItem(TagItem tagItem, out bool result)
+		{
+			tagItem.Guid = Guid.NewGuid();
+
+			result = DBTagItem.Create(
+				tagItem.Guid,
+				tagItem.SiteGuid,
+				tagItem.FeatureGuid,
+				tagItem.ModuleGuid,
+				tagItem.ItemGuid,
+				tagItem.TagGuid,
+				tagItem.ExtraGuid,
+				tagItem.TaggedBy
+			);
+
+			return tagItem;
+		}
+
+		public bool DeleteTagItemByGuid(Guid guid)
+		{
+			return DBTagItem.Delete(guid);
+		}
+
+		public bool DeleteTagItemByItemGuid(Guid itemGuid)
+		{
+			return DBTagItem.DeleteByItem(itemGuid);
+		}
+
+		public bool DeleteTagItemByExtraGuid(Guid extraGuid)
+		{
+			return DBTagItem.DeleteByExtraGuid(extraGuid);
+		}
+
+		public bool DeleteTagItemByTagGuid(Guid tagGuid)
+		{
+			return DBTagItem.DeleteByTag(tagGuid);
+		}
+
+		public bool DeleteTagItemByModuleGuid(Guid moduleGuid)
+		{
+			return DBTagItem.DeleteByModule(moduleGuid);
+		}
+
+		public bool DeleteTagItemByFeatureGuid(Guid featureGuid)
+		{
+			return DBTagItem.DeleteByFeature(featureGuid);
+		}
+
+		public bool DeleteTagItemBySiteGuid(Guid siteGuid)
+		{
+			return DBTagItem.DeleteBySite(siteGuid);
+		}
+
+		#endregion
+
+
+		#region Private Methods
+
+		private Tag getTagFromIDataReader(IDataReader reader)
+		{
+			if (reader.Read())
+			{
+				return new Tag
+				{
+					Guid = new Guid(reader["Guid"].ToString()),
+					SiteGuid = new Guid(reader["SiteGuid"].ToString()),
+					FeatureGuid = new Guid(reader["FeatureGuid"].ToString()),
+					ModuleGuid = new Guid(reader["ModuleGuid"].ToString()),
+					TagText = reader["Tax"].ToString(),
+					ItemCount = Convert.ToInt32(reader["Tag"]),
+					CreatedUtc = Convert.ToDateTime(reader["CreatedUtc"]),
+					CreatedBy = new Guid(reader["CreatedBy"].ToString()),
+					ModifiedUtc = Convert.ToDateTime(reader["ModifiedUtc"]),
+					ModifiedBy = new Guid(reader["ModifiedBy"].ToString()),
+					VocabularyGuid = new Guid(reader["VocabularyGuid"].ToString())
+				};
+			}
+
+			return null;
+		}
+
+		#endregion
+	}
 }
