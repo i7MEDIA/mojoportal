8d284caac2473ce4372957441d66d9cda224ec59
Joe Davis <joe@i7media.net>
2022-11-29 10:35:20 -0600

    Core: Added Newtonsoft Serializer

---
+++ mojoPortal.Core/Serializers/Newtonsoft.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace mojoPortal.Core.Serializers.Newtonsoft
+{
+	public class SingleOrArrayConverter<T> : JsonConverter
+	{
+		public override bool CanConvert(Type objectType)
+		{
+			return (objectType == typeof(List<T>));
+		}
+
+		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+		{
+			JToken token = JToken.Load(reader);
+			if (token.Type == JTokenType.Array)
+			{
+				return token.ToObject<List<T>>();
+			}
+			return new List<T> { token.ToObject<T>() };
+		}
+
+		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+		{
+			List<T> list = (List<T>)value;
+			if (list.Count == 1)
+			{
+				value = list[0];
+			}
+			serializer.Serialize(writer, value);
+		}
+	}
+}
