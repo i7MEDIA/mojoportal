341a612aee1664a5fba02e75f340527f6b7a20bd
Joe Davis <joe@i7media.net>
2023-03-15 11:35:45 -0500

    Core: Added ListExtensions with ToDataTable

---
+++ mojoPortal.Core/Extensions/ListExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mojoPortal.Core.Extensions
+{
+	public static class ListExtensions
+	{
+		public static DataTable ToDataTable<T>(this List<T> items)
+		{
+			DataTable dataTable = new(typeof(T).Name);
+			//Get all the properties
+			PropertyInfo[] Props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+			foreach (PropertyInfo prop in Props)
+			{
+				//Setting column names as Property names
+				dataTable.Columns.Add(prop.Name);
+			}
+			foreach (T item in items)
+			{
+				var values = new object[Props.Length];
+				for (int i = 0; i < Props.Length; i++)
+				{
+					//inserting property values to datatable rows
+					values[i] = Props[i].GetValue(item, null);
+				}
+				dataTable.Rows.Add(values);
+			}
+			//put a breakpoint here and check datatable
+			return dataTable;
+		}
+	}
+}
