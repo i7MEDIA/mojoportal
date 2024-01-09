d889ef3d595f3fbf1a548e1caadcd2c0c85ab8d2
Joseph Fowler <josephefowler@gmail.com>
2024-01-09 14:11:28 -0600

    Updated for MySQL Connector

--- mojoPortal.Data.MySql/ConnectionString.cs
+++ mojoPortal.Data.MySql/ConnectionString.cs
@@ -1,36 +1,30 @@
 ﻿using System.Configuration;
 
+namespace mojoPortal.Data;
 
-namespace mojoPortal.Data
+public static class ConnectionString
 {
-	public static class ConnectionString
+	private const string connectionString = "MySqlConnectionString";
+	private const string writeString = "MySqlWriteConnectionString";
+
+	public static string GetReadConnectionString() => GetString();
+
+	public static string GetWriteConnectionString() => GetString("write");
+
+	private static string GetString(string type = "")
 	{
-		private const string connectionString = "MySqlConnectionString";
-		private const string writeString = "MySqlWriteConnectionString";
-
-
-		public static string GetReadConnectionString() => GetString();
-
-
-		public static string GetWriteConnectionString() => GetString("write");
-
-
-		private static string GetString(string type = "")
+		if (type == "write" && ConfigurationManager.AppSettings[writeString] is not null)
 		{
-			if (type == "write" && ConfigurationManager.AppSettings[writeString] != null)
-			{
-				return ConfigurationManager.AppSettings[writeString].AddSslMode();
-			}
-
-			return ConfigurationManager.AppSettings[connectionString].AddSslMode();
+			return ConfigurationManager.AppSettings[writeString].AddSslMode();
 		}
 
+		return ConfigurationManager.AppSettings[connectionString].AddSslMode();
+	}
 
-		private static string AddSslMode(this string cstring)
-		{
-			return cstring.Contains("SslMode")
-				? cstring
-				: cstring.TrimEnd(';') + ";SslMode=none;";
-		}
+	private static string AddSslMode(this string cstring)
+	{
+		return cstring.Contains("SslMode")
+			? cstring
+			: cstring.TrimEnd(';') + ";SslMode=none;";
 	}
 }
