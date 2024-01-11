025c9e8de1ea105db8420944eb4fc56d306a0548
Joseph Fowler <josephefowler@gmail.com>
2024-01-11 11:05:38 -0600

    Fixes for MySQLConnector

--- mojoPortal.Data.MySql/DBEmailSendLog.cs
+++ mojoPortal.Data.MySql/DBEmailSendLog.cs
@@ -1,262 +1,286 @@
-﻿// Author:					
-// Created:					2009-02-23
-// Last Modified:			2012-07-20
-// 
-// The use and distribution terms for this software are covered by the 
-// Common Public License 1.0 (http://opensource.org/licenses/cpl.php)  
-// which can be found in the file CPL.TXT at the root of this distribution.
-// By using this software in any fashion, you are agreeing to be bound by 
-// the terms of this license.
-//
-// You must not remove this notice, or any other, from this software.
-
-using System;
-using System.Text;
+﻿using System;
+using System.Collections.Generic;
 using System.Data;
-using System.Data.Common;
-using System.Configuration;
-using System.Globalization;
-using System.IO;
 using MySqlConnector;
 
 namespace mojoPortal.Data
 {
-    public static class DBEmailSendLog
-    {
-       
-        /// <summary>
-        /// Inserts a row in the mp_EmailSendLog table. Returns rows affected count.
-        /// </summary>
-        /// <param name="guid"> guid </param>
-        /// <param name="siteGuid"> siteGuid </param>
-        /// <param name="moduleGuid"> moduleGuid </param>
-        /// <param name="specialGuid1"> specialGuid1 </param>
-        /// <param name="specialGuid2"> specialGuid2 </param>
-        /// <param name="toAddress"> toAddress </param>
-        /// <param name="ccAddress"> ccAddress </param>
-        /// <param name="bccAddress"> bccAddress </param>
-        /// <param name="subject"> subject </param>
-        /// <param name="textBody"> textBody </param>
-        /// <param name="htmlBody"> htmlBody </param>
-        /// <param name="type"> type </param>
-        /// <param name="sentUtc"> sentUtc </param>
-        /// <param name="fromAddress"> fromAddress </param>
-        /// <param name="replyTo"> replyTo </param>
-        /// <param name="userGuid"> userGuid </param>
-        /// <returns>int</returns>
-        public static void Create(
-            Guid guid,
-            Guid siteGuid,
-            Guid moduleGuid,
-            Guid specialGuid1,
-            Guid specialGuid2,
-            string toAddress,
-            string ccAddress,
-            string bccAddress,
-            string subject,
-            string textBody,
-            string htmlBody,
-            string type,
-            DateTime sentUtc,
-            string fromAddress,
-            string replyTo,
-            Guid userGuid)
-        {
-            StringBuilder sqlCommand = new StringBuilder();
-            sqlCommand.Append("INSERT INTO mp_EmailSendLog (");
-            sqlCommand.Append("Guid, ");
-            sqlCommand.Append("SiteGuid, ");
-            sqlCommand.Append("ModuleGuid, ");
-            sqlCommand.Append("SpecialGuid1, ");
-            sqlCommand.Append("SpecialGuid2, ");
-            sqlCommand.Append("ToAddress, ");
-            sqlCommand.Append("CcAddress, ");
-            sqlCommand.Append("BccAddress, ");
-            sqlCommand.Append("Subject, ");
-            sqlCommand.Append("TextBody, ");
-            sqlCommand.Append("HtmlBody, ");
-            sqlCommand.Append("Type, ");
-            sqlCommand.Append("SentUtc, ");
-            sqlCommand.Append("FromAddress, ");
-            sqlCommand.Append("ReplyTo, ");
-            sqlCommand.Append("UserGuid )");
+	public static class DBEmailSendLog
+	{
 
-            sqlCommand.Append(" VALUES (");
-            sqlCommand.Append("?Guid, ");
-            sqlCommand.Append("?SiteGuid, ");
-            sqlCommand.Append("?ModuleGuid, ");
-            sqlCommand.Append("?SpecialGuid1, ");
-            sqlCommand.Append("?SpecialGuid2, ");
-            sqlCommand.Append("?ToAddress, ");
-            sqlCommand.Append("?CcAddress, ");
-            sqlCommand.Append("?BccAddress, ");
-            sqlCommand.Append("?Subject, ");
-            sqlCommand.Append("?TextBody, ");
-            sqlCommand.Append("?HtmlBody, ");
-            sqlCommand.Append("?Type, ");
-            sqlCommand.Append("?SentUtc, ");
-            sqlCommand.Append("?FromAddress, ");
-            sqlCommand.Append("?ReplyTo, ");
-            sqlCommand.Append("?UserGuid )");
-            sqlCommand.Append(";");
+		/// <summary>
+		/// Inserts a row in the mp_EmailSendLog table. Returns rows affected count.
+		/// </summary>
+		/// <param name="guid"> guid </param>
+		/// <param name="siteGuid"> siteGuid </param>
+		/// <param name="moduleGuid"> moduleGuid </param>
+		/// <param name="specialGuid1"> specialGuid1 </param>
+		/// <param name="specialGuid2"> specialGuid2 </param>
+		/// <param name="toAddress"> toAddress </param>
+		/// <param name="ccAddress"> ccAddress </param>
+		/// <param name="bccAddress"> bccAddress </param>
+		/// <param name="subject"> subject </param>
+		/// <param name="textBody"> textBody </param>
+		/// <param name="htmlBody"> htmlBody </param>
+		/// <param name="type"> type </param>
+		/// <param name="sentUtc"> sentUtc </param>
+		/// <param name="fromAddress"> fromAddress </param>
+		/// <param name="replyTo"> replyTo </param>
+		/// <param name="userGuid"> userGuid </param>
+		/// <returns>int</returns>
+		public static void Create(
+			Guid guid,
+			Guid siteGuid,
+			Guid moduleGuid,
+			Guid specialGuid1,
+			Guid specialGuid2,
+			string toAddress,
+			string ccAddress,
+			string bccAddress,
+			string subject,
+			string textBody,
+			string htmlBody,
+			string type,
+			DateTime sentUtc,
+			string fromAddress,
+			string replyTo,
+			Guid userGuid)
+		{
+			string sqlCommand = @"
+INSERT INTO 
+    mp_EmailSendLog (
+        Guid, 
+        SiteGuid, 
+        ModuleGuid, 
+        SpecialGuid1, 
+        SpecialGuid2, 
+        ToAddress, 
+        CcAddress, 
+        BccAddress, 
+        Subject, 
+        TextBody, 
+        HtmlBody, 
+        Type, 
+        SentUtc, 
+        FromAddress, 
+        ReplyTo, 
+        UserGuid 
+    ) 
+VALUES (
+    ?Guid, 
+    ?SiteGuid, 
+    ?ModuleGuid, 
+    ?SpecialGuid1, 
+    ?SpecialGuid2, 
+    ?ToAddress, 
+    ?CcAddress, 
+    ?BccAddress, 
+    ?Subject, 
+    ?TextBody, 
+    ?HtmlBody, 
+    ?Type, 
+    ?SentUtc, 
+    ?FromAddress, 
+    ?ReplyTo, 
+    ?UserGuid 
+);";
 
-            MySqlParameter[] arParams = new MySqlParameter[16];
+			var arParams = new List<MySqlParameter>
+			{
+				new("?Guid", MySqlDbType.VarChar, 36)
+				{
+					Direction = ParameterDirection.Input,
+					Value = guid.ToString()
+				},
 
-            arParams[0] = new MySqlParameter("?Guid", MySqlDbType.VarChar, 36);
-            arParams[0].Direction = ParameterDirection.Input;
-            arParams[0].Value = guid.ToString();
+				new("?SiteGuid", MySqlDbType.VarChar, 36)
+				{
+					Direction = ParameterDirection.Input,
+					Value = siteGuid.ToString()
+				},
 
-            arParams[1] = new MySqlParameter("?SiteGuid", MySqlDbType.VarChar, 36);
-            arParams[1].Direction = ParameterDirection.Input;
-            arParams[1].Value = siteGuid.ToString();
+				new("?ModuleGuid", MySqlDbType.VarChar, 36)
+				{
+					Direction = ParameterDirection.Input,
+					Value = moduleGuid.ToString()
+				},
 
-            arParams[2] = new MySqlParameter("?ModuleGuid", MySqlDbType.VarChar, 36);
-            arParams[2].Direction = ParameterDirection.Input;
-            arParams[2].Value = moduleGuid.ToString();
+				new("?SpecialGuid1", MySqlDbType.VarChar, 36)
+				{
+					Direction = ParameterDirection.Input,
+					Value = specialGuid1.ToString()
+				},
 
-            arParams[3] = new MySqlParameter("?SpecialGuid1", MySqlDbType.VarChar, 36);
-            arParams[3].Direction = ParameterDirection.Input;
-            arParams[3].Value = specialGuid1.ToString();
+				new("?SpecialGuid2", MySqlDbType.VarChar, 36)
+				{
+					Direction = ParameterDirection.Input,
+					Value = specialGuid2.ToString()
+				},
 
-            arParams[4] = new MySqlParameter("?SpecialGuid2", MySqlDbType.VarChar, 36);
-            arParams[4].Direction = ParameterDirection.Input;
-            arParams[4].Value = specialGuid2.ToString();
+				new("?ToAddress", MySqlDbType.VarChar, 255)
+				{
+					Direction = ParameterDirection.Input,
+					Value = toAddress
+				},
 
-            arParams[5] = new MySqlParameter("?ToAddress", MySqlDbType.VarChar, 255);
-            arParams[5].Direction = ParameterDirection.Input;
-            arParams[5].Value = toAddress;
+				new("?CcAddress", MySqlDbType.VarChar, 255)
+				{
+					Direction = ParameterDirection.Input,
+					Value = ccAddress
+				},
 
-            arParams[6] = new MySqlParameter("?CcAddress", MySqlDbType.VarChar, 255);
-            arParams[6].Direction = ParameterDirection.Input;
-            arParams[6].Value = ccAddress;
+				new("?BccAddress", MySqlDbType.VarChar, 255)
+				{
+					Direction = ParameterDirection.Input,
+					Value = bccAddress
+				},
 
-            arParams[7] = new MySqlParameter("?BccAddress", MySqlDbType.VarChar, 255);
-            arParams[7].Direction = ParameterDirection.Input;
-            arParams[7].Value = bccAddress;
+				new("?Subject", MySqlDbType.VarChar, 255)
+				{
+					Direction = ParameterDirection.Input,
+					Value = subject
+				},
 
-            arParams[8] = new MySqlParameter("?Subject", MySqlDbType.VarChar, 255);
-            arParams[8].Direction = ParameterDirection.Input;
-            arParams[8].Value = subject;
+				new("?TextBody", MySqlDbType.LongText)
+				{
+					Direction = ParameterDirection.Input,
+					Value = textBody
+				},
 
-            arParams[9] = new MySqlParameter("?TextBody", MySqlDbType.LongText);
-            arParams[9].Direction = ParameterDirection.Input;
-            arParams[9].Value = textBody;
+				new("?HtmlBody", MySqlDbType.LongText)
+				{
+					Direction = ParameterDirection.Input,
+					Value = htmlBody
+				},
 
-            arParams[10] = new MySqlParameter("?HtmlBody", MySqlDbType.LongText);
-            arParams[10].Direction = ParameterDirection.Input;
-            arParams[10].Value = htmlBody;
+				new("?Type", MySqlDbType.VarChar, 50)
+				{
+					Direction = ParameterDirection.Input,
+					Value = type
+				},
 
-            arParams[11] = new MySqlParameter("?Type", MySqlDbType.VarChar, 50);
-            arParams[11].Direction = ParameterDirection.Input;
-            arParams[11].Value = type;
+				new("?SentUtc", MySqlDbType.DateTime)
+				{
+					Direction = ParameterDirection.Input,
+					Value = sentUtc
+				},
 
-            arParams[12] = new MySqlParameter("?SentUtc", MySqlDbType.DateTime);
-            arParams[12].Direction = ParameterDirection.Input;
-            arParams[12].Value = sentUtc;
+				new("?FromAddress", MySqlDbType.VarChar, 100)
+				{
+					Direction = ParameterDirection.Input,
+					Value = fromAddress
+				},
 
-            arParams[13] = new MySqlParameter("?FromAddress", MySqlDbType.VarChar, 100);
-            arParams[13].Direction = ParameterDirection.Input;
-            arParams[13].Value = fromAddress;
+				new("?ReplyTo", MySqlDbType.VarChar, 100)
+				{
+					Direction = ParameterDirection.Input,
+					Value = replyTo
+				},
 
-            arParams[14] = new MySqlParameter("?ReplyTo", MySqlDbType.VarChar, 100);
-            arParams[14].Direction = ParameterDirection.Input;
-            arParams[14].Value = replyTo;
-
-            arParams[15] = new MySqlParameter("?UserGuid", MySqlDbType.VarChar, 36);
-            arParams[15].Direction = ParameterDirection.Input;
-            arParams[15].Value = userGuid.ToString();
-
-            CommandHelper.ExecuteNonQuery(
-                ConnectionString.GetWriteConnectionString(),
-                sqlCommand.ToString(),
-                arParams);
-           
-
-        }
-
-        /// <summary>
-        /// Deletes a row from the mp_EmailSendLog table. Returns true if row deleted.
-        /// </summary>
-        /// <param name="guid"> guid </param>
-        /// <returns>bool</returns>
-        public static bool Delete(Guid guid)
-        {
-            StringBuilder sqlCommand = new StringBuilder();
-            sqlCommand.Append("DELETE FROM mp_EmailSendLog ");
-            sqlCommand.Append("WHERE ");
-            sqlCommand.Append("Guid = ?Guid ");
-            sqlCommand.Append(";");
-
-            MySqlParameter[] arParams = new MySqlParameter[1];
-
-            arParams[0] = new MySqlParameter("?Guid", MySqlDbType.VarChar, 36);
-            arParams[0].Direction = ParameterDirection.Input;
-            arParams[0].Value = guid.ToString();
-
-            int rowsAffected = CommandHelper.ExecuteNonQuery(
-                ConnectionString.GetWriteConnectionString(),
-                sqlCommand.ToString(),
-                arParams);
-
-            return (rowsAffected > 0);
-        }
-
-        /// <summary>
-        /// Deletes rows from the mp_EmailSendLog table. Returns true if row deleted.
-        /// </summary>
-        /// <param name="guid"> guid </param>
-        /// <returns>bool</returns>
-        public static bool DeleteBySite(Guid siteGuid)
-        {
-            StringBuilder sqlCommand = new StringBuilder();
-            sqlCommand.Append("DELETE FROM mp_EmailSendLog ");
-            sqlCommand.Append("WHERE ");
-            sqlCommand.Append("SiteGuid = ?SiteGuid ");
-            sqlCommand.Append(";");
-
-            MySqlParameter[] arParams = new MySqlParameter[1];
-
-            arParams[0] = new MySqlParameter("?SiteGuid", MySqlDbType.VarChar, 36);
-            arParams[0].Direction = ParameterDirection.Input;
-            arParams[0].Value = siteGuid.ToString();
-
-            int rowsAffected = CommandHelper.ExecuteNonQuery(
-                ConnectionString.GetWriteConnectionString(),
-                sqlCommand.ToString(),
-                arParams);
-
-            return (rowsAffected > 0);
-
-        }
-
-        /// <summary>
-        /// Deletes rows from the mp_EmailSendLog table. Returns true if row deleted.
-        /// </summary>
-        /// <param name="guid"> guid </param>
-        /// <returns>bool</returns>
-        public static bool DeleteByModule(Guid moduleGuid)
-        {
-            StringBuilder sqlCommand = new StringBuilder();
-            sqlCommand.Append("DELETE FROM mp_EmailSendLog ");
-            sqlCommand.Append("WHERE ");
-            sqlCommand.Append("ModuleGuid = ?ModuleGuid ");
-            sqlCommand.Append(";");
-
-            MySqlParameter[] arParams = new MySqlParameter[1];
-
-            arParams[0] = new MySqlParameter("?ModuleGuid", MySqlDbType.VarChar, 36);
-            arParams[0].Direction = ParameterDirection.Input;
-            arParams[0].Value = moduleGuid.ToString();
-
-            int rowsAffected = CommandHelper.ExecuteNonQuery(
-                ConnectionString.GetWriteConnectionString(),
-                sqlCommand.ToString(),
-                arParams);
-
-            return (rowsAffected > 0);
-
-        }
+				new("?UserGuid", MySqlDbType.VarChar, 36)
+				{
+					Direction = ParameterDirection.Input,
+					Value = userGuid.ToString()
+				}
+			};
 
 
-    }
+			CommandHelper.ExecuteNonQuery(
+				ConnectionString.GetWriteConnectionString(),
+				sqlCommand.ToString(),
+				arParams);
+
+
+		}
+
+		/// <summary>
+		/// Deletes a row from the mp_EmailSendLog table. Returns true if row deleted.
+		/// </summary>
+		/// <param name="guid"> guid </param>
+		/// <returns>bool</returns>
+		public static bool Delete(Guid guid)
+		{
+			string sqlCommand = @"
+DELETE FROM mp_EmailSendLog 
+WHERE Guid = ?Guid;";
+
+			var arParams = new List<MySqlParameter>
+			{
+				new("?Guid", MySqlDbType.VarChar, 36)
+				{
+					Direction = ParameterDirection.Input,
+					Value = guid.ToString()
+				}
+			};
+
+
+			int rowsAffected = CommandHelper.ExecuteNonQuery(
+				ConnectionString.GetWriteConnectionString(),
+				sqlCommand.ToString(),
+				arParams);
+
+			return rowsAffected > 0;
+		}
+
+		/// <summary>
+		/// Deletes rows from the mp_EmailSendLog table. Returns true if row deleted.
+		/// </summary>
+		/// <param name="guid"> guid </param>
+		/// <returns>bool</returns>
+		public static bool DeleteBySite(Guid siteGuid)
+		{
+			string sqlCommand = @"
+DELETE FROM mp_EmailSendLog 
+WHERE SiteGuid = ?SiteGuid;";
+
+			var arParams = new List<MySqlParameter>
+			{
+				new("?SiteGuid", MySqlDbType.VarChar, 36){
+					Direction = ParameterDirection.Input,
+					Value = siteGuid.ToString()
+				}
+		};
+
+
+			int rowsAffected = CommandHelper.ExecuteNonQuery(
+				ConnectionString.GetWriteConnectionString(),
+				sqlCommand.ToString(),
+				arParams);
+
+			return rowsAffected > 0;
+
+		}
+
+		/// <summary>
+		/// Deletes rows from the mp_EmailSendLog table. Returns true if row deleted.
+		/// </summary>
+		/// <param name="guid"> guid </param>
+		/// <returns>bool</returns>
+		public static bool DeleteByModule(Guid moduleGuid)
+		{
+			string sqlCommand = @"
+DELETE FROM mp_EmailSendLog 
+WHERE ModuleGuid = ?ModuleGuid;";
+
+			var arParams = new List<MySqlParameter>
+			{
+				new("?ModuleGuid", MySqlDbType.VarChar, 36)
+				{
+					Direction = ParameterDirection.Input,
+					Value = moduleGuid.ToString()
+				}
+			};
+
+
+			int rowsAffected = CommandHelper.ExecuteNonQuery(
+				ConnectionString.GetWriteConnectionString(),
+				sqlCommand.ToString(),
+				arParams);
+
+			return rowsAffected > 0;
+
+		}
+
+
+	}
 }
