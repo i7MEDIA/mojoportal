b939391bfe04c1045459ea5bfac05e8a1ae68eee
Joe Davis <joe@i7media.net>
2024-02-13 20:47:38 -0600

    Add FileSystemHelper

---
+++ Web/Components/FileSystem/FileSystemHelper.cs
@@ -0,0 +1,28 @@
+﻿using log4net;
+using mojoPortal.Web;
+
+namespace mojoPortal.FileSystem;
+
+public static class FileSystemHelper
+{
+	private static readonly ILog log = LogManager.GetLogger(typeof(FileSystemHelper));
+
+	public static IFileSystem LoadFileSystem(string providerName = null)
+	{
+		providerName ??= WebConfigSettings.FileSystemProvider;
+
+		FileSystemProvider p = FileSystemManager.Providers[providerName];
+
+		if (p == null)
+		{
+			log.Fatal(string.Format($"FileSystemProvider Could Not Be Loaded: {providerName}"));
+		}
+
+		IFileSystem fileSystem = p.GetFileSystem();
+		if (fileSystem == null)
+		{
+			log.Fatal(string.Format($"FileSystemProvider Could Not Be Loaded: {providerName}"));
+		}
+		return fileSystem;
+	}
+}
