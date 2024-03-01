00626a618e4270387294aa60c7849847a004f688
Joe Davis <joe@i7media.net>
2024-03-01 17:42:14 -0600

    Remove Woopra from DialogMaster

--- Web/App_MasterPages/DialogMaster.Master.designer.cs
+++ Web/App_MasterPages/DialogMaster.Master.designer.cs
@@ -85,14 +85,5 @@ namespace mojoPortal.Web
 		/// To modify move field declaration from designer file to code-behind file.
 		/// </remarks>
 		protected global::System.Web.UI.WebControls.ContentPlaceHolder phMain;
-
-		/// <summary>
-		/// woopra11 control.
-		/// </summary>
-		/// <remarks>
-		/// Auto-generated field.
-		/// To modify move field declaration from designer file to code-behind file.
-		/// </remarks>
-		protected global::mojoPortal.Web.UI.WoopraScript woopra11;
 	}
 }
