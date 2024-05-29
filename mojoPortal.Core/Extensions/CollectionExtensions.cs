b1a4ddb4e2be372589bd68d066a38b73a8696f7d
Joe Davis <joe@i7media.net>
2024-05-29 09:29:21 -0500

    Core: Consolidated ListExtensions and CollectionExtensions

--- mojoPortal.Core/Extensions/CollectionExtensions.cs
+++ mojoPortal.Core/Extensions/CollectionExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Data;
 using System.Linq;
+using System.Reflection;
 using System.Web.UI.WebControls;
 
 namespace mojoPortal.Core.Extensions;
@@ -39,9 +41,16 @@ public static class CollectionExtensions
 	}
 
 
+	/// <summary>
+	/// Creates a delimited string from a ListItemCollection
+	/// </summary>
+	/// <param name="list"></param>
+	/// <param name="itemDelimiter"></param>
+	/// <param name="addTrailingDelimiter"></param>
+	/// <returns>A delimited string of values from the collection passed to it.</returns>
 	public static string ToDelimitedString(this ListItemCollection list, string itemDelimiter = ",", bool addTrailingDelimiter = false)
 	{
-		if (list == null)
+		if (list is null)
 		{
 			return string.Empty;
 		}
@@ -49,5 +58,30 @@ public static class CollectionExtensions
 		IEnumerable<ListItem> listItems = list.Cast<ListItem>().ToList();
 
 		return string.Join(itemDelimiter, listItems.Where(x => x.Selected).Select(x => x.Value.ToString())) + (addTrailingDelimiter ? itemDelimiter : string.Empty);
+	}
+
+
+	public static DataTable ToDataTable<T>(this List<T> items)
+	{
+		DataTable dataTable = new(typeof(T).Name);
+		//Get all the properties
+		PropertyInfo[] Props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+		foreach (PropertyInfo prop in Props)
+		{
+			//Setting column names as Property names
+			dataTable.Columns.Add(prop.Name);
+		}
+		foreach (T item in items)
+		{
+			var values = new object[Props.Length];
+			for (int i = 0; i < Props.Length; i++)
+			{
+				//inserting property values to datatable rows
+				values[i] = Props[i].GetValue(item, null);
+			}
+			dataTable.Rows.Add(values);
+		}
+		//put a breakpoint here and check datatable
+		return dataTable;
 	}
 }
