caf28bdacc19ad85f525143c655ad554d19e598b
Joe Davis <joe@i7media.net>
2024-08-27 13:50:53 -0500

    Cleanup LDAP code

--- mojoPortal.Business/LdapSettings.cs
+++ mojoPortal.Business/LdapSettings.cs
@@ -1,68 +1,17 @@
-// Author:					TJ Fontaine
-// Created:				    2005-09-30
-// Last Modified:			2009-01-09
-// 
-// The use and distribution terms for this software are covered by the 
-// Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
-// which can be found in the file CPL.TXT at the root of this distribution.
-// By using this software in any fashion, you are agreeing to be bound by 
-// the terms of this license.
-//
-// You must not remove this notice, or any other, from this software. 
+namespace mojoPortal.Business;
 
-using System;
-
-namespace mojoPortal.Business
+public class LdapSettings
 {
-	/// <summary>
-	///
-	/// </summary>
-	public class LdapSettings
-	{
-		private string rootDN = String.Empty;
-		private string server = String.Empty;
+	public LdapSettings() { }
 
-        // TODO: get from db
-        private String domain = String.Empty;
-		private int port = 389;
-		// in open ldap this would be uid, for AD CN
-		private String userDNKey = "CN";
+	public string Server { get; set; } = string.Empty;
 
-		public LdapSettings()
-		{
-		}
+	public string Domain { get; set; } = string.Empty;
 
-		public string Server
-		{
-			get { return server; }
-			set { server = value;}
-		}
+	public int Port { get; set; } = 389;
 
-        public string Domain
-        {
-            get { return domain; }
-            set { domain = value; }
-        }
+	public string RootDN { get; set; } = string.Empty;
 
-		public int Port
-		{
-			get { return port; }
-			set { port = value;}
-		}
+	public string UserDNKey { get; set; } = "CN";
 
-		
-		public string RootDN
-		{
-			get { return rootDN; }
-			set { rootDN = value;}
-		}
-
-		public string UserDNKey
-		{
-			get { return userDNKey; }
-			set { userDNKey = value;}
-		}
-
-		
-	}
 }
--- mojoPortal.Business/LdapUser.cs
+++ mojoPortal.Business/LdapUser.cs
@@ -1,172 +1,129 @@
-// Author:					TJ Fontaine
-// Created:				    2005-09-30
-// Last Modified:			2012-06-07
-// 
-// 1/7/2006 Haluk Eryuksel added convNull2Blank
-// 
-// The use and distribution terms for this software are covered by the 
-// Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
-// which can be found in the file CPL.TXT at the root of this distribution.
-// By using this software in any fashion, you are agreeing to be bound by 
-// the terms of this license.
-//
-// You must not remove this notice, or any other, from this software. 
-// 2011-02-05 added changes from Jamie Eubanks to better support Active Directory
-
-using System;
-using Novell.Directory.Ldap;
 using System.DirectoryServices;
+using Novell.Directory.Ldap;
 
-namespace mojoPortal.Business
+namespace mojoPortal.Business;
+
+public class LdapUser
 {
-	/// <summary>
-	///
-	/// </summary>
-	public class LdapUser
+	private LdapAttribute email = null;
+	private LdapAttribute commonname = null;
+	private LdapAttribute password = null;
+	private LdapAttribute userid = null;
+	private LdapAttribute uidNumber = null;
+	private string dn = String.Empty;
+	private string firstName = string.Empty;
+	private string lastName = string.Empty;
+
+
+	public LdapUser() { }
+
+
+	public LdapUser(DirectoryEntry adentry, String userName, LdapSettings ldapSettings)
 	{
-		private LdapAttribute email = null;
-		private LdapAttribute commonname = null;
-		private LdapAttribute password = null;
-		private LdapAttribute userid = null;
-		private LdapAttribute uidNumber = null;
-		private string dn = String.Empty;
-        private string firstName = string.Empty;
-        private string lastName = string.Empty;
-
-		public LdapUser() {}
-
-        
-        public LdapUser(DirectoryEntry adentry, String userName, LdapSettings ldapSettings)
-        {
-            userid = new LdapAttribute("userid", userName);
-            DirectorySearcher ds = new DirectorySearcher(adentry);
-            ds.Filter = "(&(sAMAccountName=" + userName + "))";
-            SearchResult result = ds.FindOne();
-            DirectoryEntry ent = null;
-
-            if (result != null)
-            {
-                ent = result.GetDirectoryEntry();
-            }
-
-            if (ent != null)
-            {
-                if (ent.Properties["cn"].Value != null)
-                {
-                    commonname = new LdapAttribute("commonname", ent.Properties["cn"].Value.ToString());
-                }
-                else
-                {
-                    commonname = new LdapAttribute("commonname", userName);
-                }
-                if (ent.Properties["mail"].Value != null)
-                {
-                    email = new LdapAttribute("email", ent.Properties["mail"].Value.ToString());
-                }
-                else
-                {
-                    email = new LdapAttribute("email", userName + "@" + ldapSettings.Domain);
-                }
-            }
-        }
-
-        //public LdapUser(LdapSettings ldapSettings, String userName)
-        //{
-        //    // in some cases with Active Directory
-        //    // we can't actually retrieve ldap entries
-        //    // we really just need to create a mojoportal user
-        //    // from the ldap user so if we can't read it, just create an ldap user
-        //    // with the properties we do have
-        //    // Active Directory allows us to bind a connection for authentication
-        //    // even if we can't query for entries
-
-        //    email = new LdapAttribute("email", userName + "@" + ldapSettings.Domain);
-        //    commonname = new LdapAttribute("commonname", userName);
-        //    userid = new LdapAttribute("userid", userName);
-
-        //}
-
-		public LdapUser(LdapEntry entry)
+		userid = new LdapAttribute("userid", userName);
+		
+		var ds = new DirectorySearcher(adentry)
 		{
-			dn = entry.DN;
+			Filter = "(&(sAMAccountName=" + userName + "))"
+		};
 
-			LdapAttributeSet las = entry.getAttributeSet();
-			
-			foreach(LdapAttribute a in las)
+		SearchResult result = ds.FindOne();
+
+		DirectoryEntry ent = null;
+
+		if (result != null)
+		{
+			ent = result.GetDirectoryEntry();
+		}
+
+		if (ent != null)
+		{
+			if (ent.Properties["cn"].Value != null)
 			{
-				switch(a.Name)
-				{
-					case "mail":
-						this.email = a;
-						break;
-					case "cn":
-						this.commonname = a;
-						break;
-					case "userPassword":
-						this.password = a;
-						break;
-					case "uidNumber":
-						this.uidNumber = a;
-						break;
-					case "uid":
-						this.userid = a;
-                        break;
-                    case "sAMAccountName":
-                        this.userid = a;
-						break;
-                    case "givenName":
-                        this.firstName = a.StringValue;
-                        break;
-                    case "sn":
-                        this.lastName = a.StringValue;
-                        break;
-				}
+				commonname = new LdapAttribute("commonname", ent.Properties["cn"].Value.ToString());
+			}
+			else
+			{
+				commonname = new LdapAttribute("commonname", userName);
+			}
+
+			if (ent.Properties["mail"].Value != null)
+			{
+				email = new LdapAttribute("email", ent.Properties["mail"].Value.ToString());
+			}
+			else
+			{
+				email = new LdapAttribute("email", userName + "@" + ldapSettings.Domain);
 			}
 		}
-
-		public string Email
-		{
-            get { return ConvNull2Blank(email); }
-		}
-
-		public string UserId
-		{
-			get { return ConvNull2Blank(userid); }
-		}
-
-		public string CommonName
-		{
-            get { return ConvNull2Blank(commonname); }
-
-		}
-
-		public string Password
-		{
-            get { return ConvNull2Blank(password); }
-		}
-
-		public string DN
-		{
-			get { return dn; }
-		}
-
-        public string FirstName
-        {
-            get { return firstName; }
-        }
-
-        public string LastName
-        {
-            get { return lastName; }
-        }
-
-        public String ConvNull2Blank(LdapAttribute str)
-        {
-            if (str == null)
-                return " ";
-            else
-                return str.StringValue;
-        }
-
 	}
+
+	//public LdapUser(LdapSettings ldapSettings, String userName)
+	//{
+	//    // in some cases with Active Directory
+	//    // we can't actually retrieve ldap entries
+	//    // we really just need to create a mojoportal user
+	//    // from the ldap user so if we can't read it, just create an ldap user
+	//    // with the properties we do have
+	//    // Active Directory allows us to bind a connection for authentication
+	//    // even if we can't query for entries
+
+	//    email = new LdapAttribute("email", userName + "@" + ldapSettings.Domain);
+	//    commonname = new LdapAttribute("commonname", userName);
+	//    userid = new LdapAttribute("userid", userName);
+
+	//}
+
+	public LdapUser(LdapEntry entry)
+	{
+		dn = entry.DN;
+
+		foreach (LdapAttribute a in entry.getAttributeSet())
+		{
+			switch (a.Name)
+			{
+				case "mail":
+					this.email = a;
+					break;
+				case "cn":
+					this.commonname = a;
+					break;
+				case "userPassword":
+					this.password = a;
+					break;
+				case "uidNumber":
+					this.uidNumber = a;
+					break;
+				case "uid":
+					this.userid = a;
+					break;
+				case "sAMAccountName":
+					this.userid = a;
+					break;
+				case "givenName":
+					this.firstName = a.StringValue;
+					break;
+				case "sn":
+					this.lastName = a.StringValue;
+					break;
+			}
+		}
+	}
+
+
+	public string Email => ConvNull2Blank(email);
+
+	public string UserId => ConvNull2Blank(userid);
+
+	public string CommonName => ConvNull2Blank(commonname);
+
+	public string Password => ConvNull2Blank(password);
+
+	public string DN => dn;
+
+	public string FirstName => firstName;
+
+	public string LastName => lastName;
+
+	public string ConvNull2Blank(LdapAttribute str) => str == null ? " " : str.StringValue;
 }
