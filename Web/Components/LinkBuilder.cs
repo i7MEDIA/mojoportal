185322c35853855a56c4013c620b3d47c3534b65
Elijah Fowler <j.elijah.fowler@gmail.com>
2024-09-06 20:18:02 -0500

    Cleaned up LinkBuilder

--- Web/Components/LinkBuilder.cs
+++ Web/Components/LinkBuilder.cs
@@ -5,85 +5,109 @@ namespace mojoPortal.Web;
 
 public class LinkBuilder
 {
-	private readonly string url;
-	private readonly Dictionary<string, object> queries = [];
-	private readonly bool includeSiteRoot;
+	private readonly string _url;
+	private readonly Dictionary<string, object> _queries = [];
+	private readonly bool _includeSiteRoot;
+
 
 	public LinkBuilder(string url, bool includeSiteRoot = true)
 	{
-		this.url = url;
-		if (this.url.Contains("?"))
+		_url = url;
+
+		if (_url.Contains("?"))
 		{
-			var query = HttpUtility.ParseQueryString(this.url);
+			var query = HttpUtility.ParseQueryString(_url);
 			foreach (KeyValuePair<string, string> item in query)
 			{
-				queries[item.Key] = item.Value;
+				_queries[item.Key] = item.Value;
 			}
-			this.url = this.url.Substring(0, this.url.IndexOf('?'));
+			_url = _url.Substring(0, _url.IndexOf('?'));
 		}
 
-		if (includeSiteRoot && this.url.Contains(SiteUtils.GetNavigationSiteRoot()))
+		if (includeSiteRoot && _url.Contains(SiteUtils.GetNavigationSiteRoot()))
 		{
 			includeSiteRoot = false;
 		}
 
-		this.includeSiteRoot = includeSiteRoot;
+		_includeSiteRoot = includeSiteRoot;
 	}
+
+
 	public LinkBuilder PageId(int id)
 	{
-		queries.Add("pageid", id);
-		return this;
-	}
-	public LinkBuilder ModuleId(int id)
-	{
-		queries.Add("mid", id);
-		return this;
-	}
-	public LinkBuilder SiteId(int id)
-	{
-		queries.Add("siteid", id);
-		return this;
-	}
-	public LinkBuilder ItemId(int id)
-	{
-		queries.Add("itemid", id);
-		return this;
-	}
-	public LinkBuilder ReturnUrl(string returnUrl)
-	{
-		queries.Add("returnurl", UrlEncode(returnUrl)); //UrlEncode prevents querystring from being used as vector for XSS
+		_queries.Add("pageid", id);
+
 		return this;
 	}
 
-	public LinkBuilder AddParam(string key, object value)
+
+	public LinkBuilder ModuleId(int id)
 	{
-		queries.Add(key, UrlEncode(value.ToString())); //UrlEncode prevents querystring from being used as vector for XSS
+		_queries.Add("mid", id);
+
 		return this;
 	}
+
+
+	public LinkBuilder SiteId(int id)
+	{
+		_queries.Add("siteid", id);
+
+		return this;
+	}
+
+
+	public LinkBuilder ItemId(int id)
+	{
+		_queries.Add("itemid", id);
+
+		return this;
+	}
+
+
+	public LinkBuilder ReturnUrl(string returnUrl)
+	{
+		_queries.Add("returnurl", UrlEncode(returnUrl)); //UrlEncode prevents querystring from being used as vector for XSS
+
+		return this;
+	}
+
+
+	public LinkBuilder AddParam(string key, object value)
+	{
+		_queries.Add(key, UrlEncode(value.ToString())); //UrlEncode prevents querystring from being used as vector for XSS
+
+		return this;
+	}
+
 
 	public LinkBuilder AddParams(Dictionary<string, object> @params)
 	{
 		foreach (var @param in @params)
 		{
-			queries.Add(@param.Key, UrlEncode(@param.Value.ToString())); //UrlEncode prevents querystring from being used as vector for XSS
+			_queries.Add(@param.Key, UrlEncode(@param.Value.ToString())); //UrlEncode prevents querystring from being used as vector for XSS
 		}
+
 		return this;
 	}
+
 
 	public override string ToString()
 	{
 		string siteRoot = string.Empty;
 
-		if (includeSiteRoot)
+		if (_includeSiteRoot)
 		{
 			siteRoot = SiteUtils.GetNavigationSiteRoot();
 		}
 
-		string qmark = queries.Count > 0 ? "?" : string.Empty;
+		string qmark = _queries.Count > 0 ? "?" : string.Empty;
+
 		//return string.Format(CultureInfo.InvariantCulture, $"{siteRoot}/{url.TrimStart('~', '/')}{qmark}{queries.ToDelimitedString()}".TrimStart('/'));
-		return $"{siteRoot}/{url?.TrimStart('~', '/')}{qmark}{queries.ToDelimitedString()}".TrimStart('/');
+		return $"{siteRoot}/{_url?.TrimStart('~', '/')}{qmark}{_queries.ToDelimitedString()}".TrimStart('/');
 	}
 }
+
 
 public static class LinkBuilderExtensions
 {
