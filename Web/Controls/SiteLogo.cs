74362dc87887df68e9188244395cacff6d9b2cdd
Elijah Fowler <j.elijah.fowler@gmail.com>
2024-12-31 09:54:26 -0600

    Refactored SiteLogo

--- Web/Controls/SiteLogo.cs
+++ Web/Controls/SiteLogo.cs
@@ -1,114 +1,85 @@
-// The use and distribution terms for this software are covered by the 
-// Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
-// which can be found in the file CPL.TXT at the root of this distribution.
-// By using this software in any fashion, you are agreeing to be bound by 
-// the terms of this license.
-//
-// You must not remove this notice, or any other, from this software.
-// Author:				    
-// Created:			    2004-08-26
-//	
-// 
-// 2007/04/13   Alexander Yushchenko: code refactoring, made it WebControl instead of UserControl.
-// 2012/03/16   Joe Davis: added cssClass properties to h1, link and image
-// 2014-05-07 JA
-// 2015/11/09   Joe Davis: added UseUrl option, default false
-
-using System;
+using mojoPortal.Business.WebHelpers;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using mojoPortal.Business;
-using mojoPortal.Business.WebHelpers;
 
-namespace mojoPortal.Web.UI
+namespace mojoPortal.Web.UI;
+
+public class SiteLogo : WebControl
 {
-    public class SiteLogo : WebControl
-    {
-		public bool UseH1 { get; set; } = false;
-		public string OverrideUrl { get; set; } = string.Empty;
-		public string OverrideTitle { get; set; } = string.Empty;
-		public string OverrideImageUrl { get; set; } = string.Empty;
-		public string ImageCssClass { get; set; } = "sitelogo";
-		public string LinkCssClass { get; set; } = string.Empty;
-		public string H1CssClass { get; set; } = "sitelogo";
-		public bool UseUrl { get; set; } = true;
+	public string HeadingElement { get; set; } = "h1";
+	public bool UseH1 { get; set; } = false;
+	public string OverrideUrl { get; set; } = string.Empty;
+	public string OverrideTitle { get; set; } = string.Empty;
+	public string OverrideImageUrl { get; set; } = string.Empty;
+	public string ImageCssClass { get; set; } = "sitelogo";
+	public string LinkCssClass { get; set; } = string.Empty;
+	public string H1CssClass { get; set; } = "sitelogo";
+	public bool UseUrl { get; set; } = true;
 
-		protected override void Render(HtmlTextWriter writer)
-        {
 
-            if (HttpContext.Current == null)
-            {
-                
-                writer.Write("[" + this.ID + "]");
-            }
-            else
-            {
-                SiteSettings siteSettings = CacheHelper.GetCurrentSiteSettings();
-                if (siteSettings == null || String.IsNullOrEmpty(siteSettings.Logo)) return;
+	protected override void Render(HtmlTextWriter writer)
+	{
 
-                string urlToUse = "~/";
-                string titleToUse = siteSettings.SiteName.Replace("\"","'");
-                string imageUrlToUse;
+		if (HttpContext.Current == null)
+		{
+			writer.Write($"[{ID}]");
+		}
+		else
+		{
+			var siteSettings = CacheHelper.GetCurrentSiteSettings();
 
-                if (WebConfigSettings.SiteLogoUseMediaFolder)
-                {
-                    imageUrlToUse = Page.ResolveUrl("~/Data/Sites/")
-                    + siteSettings.SiteId.ToString()
-                    + "/media/logos/" + siteSettings.Logo;
-                }
-                else
-                {
-                    imageUrlToUse = Page.ResolveUrl("~/Data/Sites/")
-                    + siteSettings.SiteId.ToString()
-                    + "/logos/" + siteSettings.Logo;
-                }
+			if (siteSettings == null || string.IsNullOrWhiteSpace(siteSettings.Logo))
+			{
+				return;
+			}
 
-                
+			var urlToUse = !string.IsNullOrWhiteSpace(OverrideUrl) ?
+				OverrideUrl.ToLinkBuilder().ToString() :
+				"~/".ToLinkBuilder().ToString();
 
-                string siteRoot = SiteUtils.GetNavigationSiteRoot(siteSettings);
+			var titleToUse = !string.IsNullOrWhiteSpace(OverrideTitle) ?
+				OverrideTitle :
+				siteSettings.SiteName.Replace("\"", "'");
 
-                if (siteSettings.SiteFolderName.Length > 0)
-                {
-                    urlToUse = "~/Default.aspx".ToLinkBuilder().ToString();
-                }
+			var mediaPath = WebConfigSettings.SiteLogoUseMediaFolder ?
+				"media/" :
+				string.Empty;
 
-                if (UseH1)
-                {
-                    writer.Write("<h1 class='{0}'>", H1CssClass);
-                }
+			var imageUrlToUse = !string.IsNullOrWhiteSpace(OverrideImageUrl) ?
+				OverrideImageUrl.ToLinkBuilder().ToString() :
+				Invariant($"~/Data/Sites/{siteSettings.SiteId}/{mediaPath}logos/{siteSettings.Logo}").ToLinkBuilder().ToString();
 
-                if (OverrideUrl.Length > 0)
-                {
-                    if (siteSettings.SiteFolderName.Length > 0)
-                    {
-                        OverrideUrl = siteRoot + OverrideUrl.Replace("~/", "/");
-                    }
-                    urlToUse = OverrideUrl;
-                }
+			if (UseH1)
+			{
+				writer.WriteBeginTag(HeadingElement);
+				writer.WriteAttribute("class", H1CssClass);
+			}
 
-                if (OverrideImageUrl.Length > 0)
-                {
-                    imageUrlToUse = Page.ResolveUrl(OverrideImageUrl);
-                }
+			if (UseUrl)
+			{
+				writer.WriteBeginTag("a");
+				writer.WriteAttribute("href", urlToUse);
+				writer.WriteAttribute("title", titleToUse);
+				writer.WriteAttribute("class", LinkCssClass);
+				writer.Write(HtmlTextWriter.TagRightChar);
+			}
 
-                if (OverrideTitle.Length > 0) titleToUse = OverrideTitle;
-                //if (cssClass == string.Empty) cssClass = "sitelogo";
-                if (UseUrl)
-                {
-                    writer.Write($"<a href=\"{Page.ResolveUrl(urlToUse)}\" title=\"{titleToUse}\" class=\"{LinkCssClass}\"><img class=\"{ImageCssClass}\" alt=\"{titleToUse}\" src=\"{imageUrlToUse}\" /></a>");
-                }
-                else
-                {
-                    writer.Write($"<img class=\"{ImageCssClass}\" alt=\"{titleToUse}\" src=\"{imageUrlToUse}\" />");
-                }
+			writer.WriteBeginTag("img");
+			writer.WriteAttribute("class", ImageCssClass);
+			writer.WriteAttribute("alt", titleToUse);
+			writer.WriteAttribute("src", imageUrlToUse);
+			writer.Write(HtmlTextWriter.SelfClosingTagEnd);
 
-                if (UseH1)
-                {
-                    writer.Write("</h1>");
-                }
-            }
+			if (UseUrl)
+			{
+				writer.WriteEndTag("a");
+			}
 
-        }
-    }
+			if (UseH1)
+			{
+				writer.WriteEndTag(HeadingElement);
+			}
+		}
+	}
 }
