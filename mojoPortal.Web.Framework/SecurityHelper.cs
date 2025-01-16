84da1d026eaabfc60ab6ed5f2c57cac95d9fb391
Elijah Fowler <j.elijah.fowler@gmail.com>
2025-01-16 16:42:05 -0600

    Web.Framework: Fixed Regex that didn't work in the browser

--- mojoPortal.Web.Framework/SecurityHelper.cs
+++ mojoPortal.Web.Framework/SecurityHelper.cs
@@ -1,3 +1,6 @@
+using Brettle.Web.NeatHtml;
+using log4net;
+using mojoPortal.Core.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Net.Mail;
@@ -5,9 +8,6 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Web;
 using System.Web.UI;
-using Brettle.Web.NeatHtml;
-using log4net;
-using mojoPortal.Core.Configuration;
 
 namespace mojoPortal.Web.Framework;
 
@@ -15,28 +15,16 @@ public static class SecurityHelper
 {
 	private static readonly ILog log = LogManager.GetLogger(typeof(SecurityHelper));
 
-	public const string RegexRelativeImageUrlPatern = @"^(?i)/.*[_a-zA-Z0-9]+\.(png|jpg|jpeg|gif|PNG|JPG|JPEG|GIF)$";
-	public const string RegexAnyImageUrlPatern = @"^(?i).*[_a-zA-Z0-9]+\.(png|jpg|jpeg|gif|PNG|JPG|JPEG|GIF)$";
-
+	public const string RegexRelativeImageUrlPatern = @"^/.*[_a-zA-Z0-9]+\.(png|jpg|jpeg|gif|PNG|JPG|JPEG|GIF)$";
+	public const string RegexAnyImageUrlPatern = @"^.*[_a-zA-Z0-9]+\.(png|jpg|jpeg|gif|PNG|JPG|JPEG|GIF)$";
 	public const string RegexAnyHttpOrHttpsUrl = @"^(http|https)://([^\s]+)/?";
 
-	//** Email Validation
 	/// <summary>
-	/// a regular expression for validating email addresses, efficient but not completely RFC 822 compliant
+	/// a regular expression for validating email addresses.
+	/// Copied from https://github.com/Microsoft/referencesource/blob/master/System.ComponentModel.DataAnnotations/DataAnnotations/EmailAddressAttribute.cs#L54
 	/// </summary>
-	public const string RegexEmailValidationPattern = @"^(?i)([0-9A-Z](['-.\w]*[_0-9A-Z])*@(([0-9A-Z])+([-\w']*[0-9A-Z])*\.)+[A-Z]{2,9})$|^$";
+	public const string RegexEmailValidationPattern = @"^((([a-zA-Z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-zA-Z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-zA-Z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-zA-Z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-zA-Z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-zA-Z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-zA-Z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-zA-Z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-zA-Z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-zA-Z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$";
 
-	// 2014-03-18 this expression hangs with patriciarichards5395@yahoo.comgetgoing
-	// but is still the best one I've found because the others fail to accept foo+test@foo.com
-	// @"^([0-9a-zA-Z](['-.\w]*[_0-9a-zA-Z])*@(([0-9a-zA-Z])+([-\w']*[0-9a-zA-Z])*\.)+[a-zA-Z]{2,9})$";
-
-	// 2014-03-18 tried this one http://stackoverflow.com/questions/16167983/best-regular-expression-for-email-validation-in-c-sharp
-	// @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z");
-	// but it doesn't accept + signs like foo+test@gmail.com
-
-	// http://stackoverflow.com/questions/201323/using-a-regular-expression-to-validate-an-email-address
-	// ^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$
-	// this one also fails to accept foo+test@foo.com
 
 	/// <summary>
 	/// it is better to use this method than to directly use the constant above becuase this allows you to override the experession if needed
@@ -46,6 +34,7 @@ public static class SecurityHelper
 	public static string GetEmailRegexExpression()
 	{
 		string overrideRegex = ConfigHelper.GetStringProperty("CustomEmailRegex", string.Empty);
+
 		if (!string.IsNullOrWhiteSpace(overrideRegex))
 		{
 			return overrideRegex;
