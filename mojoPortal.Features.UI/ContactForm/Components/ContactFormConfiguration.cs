016dfe79d4ab4f0c62a7acf2a571483fab0420fa
Joe Davis <joe@i7media.net>
2025-02-28 17:27:18 -0600

    ContactForm: File cleanup

--- mojoPortal.Features.UI/ContactForm/Components/ContactFormConfiguration.cs
+++ mojoPortal.Features.UI/ContactForm/Components/ContactFormConfiguration.cs
@@ -4,114 +4,113 @@ using System.Collections.Generic;
 using System.Configuration;
 using mojoPortal.Web.Framework;
 
-namespace mojoPortal.Web.ContactUI
+namespace mojoPortal.Web.ContactUI;
+
+public class ContactFormConfiguration
 {
-    public class ContactFormConfiguration
-    {
-         public ContactFormConfiguration()
-        { }
+	public ContactFormConfiguration()
+	{ }
 
-         public ContactFormConfiguration(Hashtable settings)
-        {
-            LoadSettings(settings);
+	public ContactFormConfiguration(Hashtable settings)
+	{
+		LoadSettings(settings);
 
-        }
+	}
 
-        private void LoadSettings(Hashtable settings)
-        {
-            if (settings == null) { throw new ArgumentException("must pass in a hashtable of settings"); }
+	private void LoadSettings(Hashtable settings)
+	{
+		if (settings == null) { throw new ArgumentException("must pass in a hashtable of settings"); }
 
-            if (settings.Contains("CustomCssClassSetting"))
-            {
-                InstanceCssClass = settings["CustomCssClassSetting"].ToString();
-            }
+		if (settings.Contains("CustomCssClassSetting"))
+		{
+			InstanceCssClass = settings["CustomCssClassSetting"].ToString();
+		}
 
-            if (settings.Contains("ContactFormEditorHeightSetting"))
-            {
-                EditorHeight = settings["ContactFormEditorHeightSetting"].ToString();
-            }
+		if (settings.Contains("ContactFormEditorHeightSetting"))
+		{
+			EditorHeight = settings["ContactFormEditorHeightSetting"].ToString();
+		}
 
-            if (settings.Contains("ContactFormSubjectPrefixSetting"))
-            {
-                subjectPrefix = settings["ContactFormSubjectPrefixSetting"].ToString().Trim();
-            }
+		if (settings.Contains("ContactFormSubjectPrefixSetting"))
+		{
+			subjectPrefix = settings["ContactFormSubjectPrefixSetting"].ToString().Trim();
+		}
 
-            if (settings.Contains("ContactFormEmailSetting"))
-            {
-                emailReceiveAddresses = settings["ContactFormEmailSetting"].ToString().Trim();
-                EmailAddresses = emailReceiveAddresses.SplitOnChar('|');
-            }
+		if (settings.Contains("ContactFormEmailSetting"))
+		{
+			emailReceiveAddresses = settings["ContactFormEmailSetting"].ToString().Trim();
+			EmailAddresses = emailReceiveAddresses.SplitOnChar('|');
+		}
 
-            if (settings.Contains("ContactFormEmailAliasSetting"))
-            {
-                emailReceiveAliases = settings["ContactFormEmailAliasSetting"].ToString().Trim();
+		if (settings.Contains("ContactFormEmailAliasSetting"))
+		{
+			emailReceiveAliases = settings["ContactFormEmailAliasSetting"].ToString().Trim();
 
-            }
+		}
 
-            EmailAliases = emailReceiveAliases.SplitOnChar('|');
+		EmailAliases = emailReceiveAliases.SplitOnChar('|');
 
-            if (settings.Contains("ContactFormEmailBccSetting"))
-            {
-                EmailBccAddresses = settings["ContactFormEmailBccSetting"].ToString();
-            }
+		if (settings.Contains("ContactFormEmailBccSetting"))
+		{
+			EmailBccAddresses = settings["ContactFormEmailBccSetting"].ToString();
+		}
 
-            if (EmailAliases == null) { EmailAliases = new List<string>(); }
+		if (EmailAliases == null) { EmailAliases = new List<string>(); }
 
-            UseSpamBlocking = WebUtils.ParseBoolFromHashtable(settings, "ContactFormUseCommentSpamBlocker", true);
-			BlockBadWords = WebUtils.ParseBoolFromHashtable(settings, "BlockBadWords", true);
+		UseSpamBlocking = WebUtils.ParseBoolFromHashtable(settings, "ContactFormUseCommentSpamBlocker", true);
+		BlockBadWords = WebUtils.ParseBoolFromHashtable(settings, "BlockBadWords", true);
 
-			AppendIPToMessageSetting = WebUtils.ParseBoolFromHashtable(settings, "AppendIPToMessageSetting", AppendIPToMessageSetting);
-            KeepMessages = WebUtils.ParseBoolFromHashtable(settings, "KeepMessagesInDatabase", KeepMessages);
-			//can't do this reliably anymore
-            //useInputAsFromAddress = WebUtils.ParseBoolFromHashtable(settings, "UseInputAddressAsFromAddress", useInputAsFromAddress);
+		AppendIPToMessageSetting = WebUtils.ParseBoolFromHashtable(settings, "AppendIPToMessageSetting", AppendIPToMessageSetting);
+		KeepMessages = WebUtils.ParseBoolFromHashtable(settings, "KeepMessagesInDatabase", KeepMessages);
+		//can't do this reliably anymore
+		//useInputAsFromAddress = WebUtils.ParseBoolFromHashtable(settings, "UseInputAddressAsFromAddress", useInputAsFromAddress);
 
-            //useHeading = WebUtils.ParseBoolFromHashtable(settings, "UseHeading", useHeading);
+		//useHeading = WebUtils.ParseBoolFromHashtable(settings, "UseHeading", useHeading);
 
-        }
+	}
 
-		public bool UseSpamBlocking { get; private set; } = false;
-		public bool BlockBadWords { get; private set; } = true;
+	public bool UseSpamBlocking { get; private set; } = false;
+	public bool BlockBadWords { get; private set; } = true;
 
-		public bool AppendIPToMessageSetting { get; private set; } = true;
+	public bool AppendIPToMessageSetting { get; private set; } = true;
 
 
-		private string emailReceiveAddresses = string.Empty;
+	private string emailReceiveAddresses = string.Empty;
 
-        private string emailReceiveAliases = string.Empty;
+	private string emailReceiveAliases = string.Empty;
 
-		public List<string> EmailAddresses { get; private set; } = null;
+	public List<string> EmailAddresses { get; private set; } = null;
 
-		public List<string> EmailAliases { get; private set; } = null;
+	public List<string> EmailAliases { get; private set; } = null;
 
-		public string EmailBccAddresses { get; private set; } = string.Empty;
+	public string EmailBccAddresses { get; private set; } = string.Empty;
 
-		public bool UseInputAsFromAddress { get; } = false;
+	public bool UseInputAsFromAddress { get; } = false;
 
-		public bool KeepMessages { get; private set; } = true;
+	public bool KeepMessages { get; private set; } = true;
 
-		private string subjectPrefix = string.Empty;
+	private string subjectPrefix = string.Empty;
 
-        public string SubjectPrefix
-        {
-            get { return subjectPrefix; }
-        }
+	public string SubjectPrefix
+	{
+		get { return subjectPrefix; }
+	}
 
-		public string EditorHeight { get; private set; } = "350";
+	public string EditorHeight { get; private set; } = "350";
 
-		public string InstanceCssClass { get; private set; } = string.Empty;
+	public string InstanceCssClass { get; private set; } = string.Empty;
 
 
-		public static string OverrideEditorProvider
-        {
-            get
-            {
-                if (ConfigurationManager.AppSettings["ContactFormOverrideEditorProvider"] != null)
-                {
-                    return ConfigurationManager.AppSettings["ContactFormOverrideEditorProvider"];
-                }
-                return string.Empty;
-            }
-        }
+	public static string OverrideEditorProvider
+	{
+		get
+		{
+			if (ConfigurationManager.AppSettings["ContactFormOverrideEditorProvider"] != null)
+			{
+				return ConfigurationManager.AppSettings["ContactFormOverrideEditorProvider"];
+			}
+			return string.Empty;
+		}
+	}
 
-    }
 }
--- mojoPortal.Features.UI/ContactForm/Components/SitePreDeleteContactFormHandler.cs
+++ mojoPortal.Features.UI/ContactForm/Components/SitePreDeleteContactFormHandler.cs
@@ -1,39 +1,18 @@
-﻿using System;
-using System.Configuration.Provider;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Text;
+﻿using log4net;
 using mojoPortal.Business;
 using mojoPortal.Business.WebHelpers;
-using log4net;
 
-namespace mojoPortal.Features
+namespace mojoPortal.Features;
+
+public class SitePreDeleteContactFormHandler : SitePreDeleteHandlerProvider
 {
-    /// <summary>
-    /// Author:                     
-    /// Created:                    2008-11-12
-    ///	Last Modified:              2008-11-12
-    /// 
-    /// The use and distribution terms for this software are covered by the 
-    /// Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
-    /// which can be found in the file CPL.TXT at the root of this distribution.
-    /// By using this software in any fashion, you are agreeing to be bound by 
-    /// the terms of this license.
-    ///
-    /// You must not remove this notice, or any other, from this software.
-    ///  
-    /// </summary>
-    public class SitePreDeleteContactFormHandler : SitePreDeleteHandlerProvider
-    {
-        private static readonly ILog log
-            = LogManager.GetLogger(typeof(SitePreDeleteContactFormHandler));
+	private static readonly ILog log = LogManager.GetLogger(typeof(SitePreDeleteContactFormHandler));
 
-        public SitePreDeleteContactFormHandler()
-        { }
+	public SitePreDeleteContactFormHandler()
+	{ }
 
-        public override void DeleteSiteContent(int siteId)
-        {
-            ContactFormMessage.DeleteBySite(siteId);
-        }
-    }
+	public override void DeleteSiteContent(int siteId)
+	{
+		ContactFormMessage.DeleteBySite(siteId);
+	}
 }
