047608d4ee00bdc03a54e4b14a6f955e69dccd27
Elijah Fowler <j.elijah.fowler@gmail.com>
2025-03-08 00:03:34 -0600

    Web.Features: Fixed Show Map bug

--- mojoPortal.Features.UI/EventCalendar/EditEvent.aspx
+++ mojoPortal.Features.UI/EventCalendar/EditEvent.aspx
@@ -1,52 +1,125 @@
 <%@ Page Language="c#" CodeBehind="EditEvent.aspx.cs" MasterPageFile="~/App_MasterPages/layout.Master" AutoEventWireup="false" Inherits="mojoPortal.Web.EventCalendarUI.EventCalendarEdit" %>
 
 <asp:Content ContentPlaceHolderID="leftContent" ID="MPLeftPane" runat="server" />
+
 <asp:Content ContentPlaceHolderID="mainContent" ID="MPContent" runat="server">
 	<portal:OuterWrapperPanel ID="pnlOuterWrap" runat="server">
 		<portal:InnerWrapperPanel ID="pnlInnerWrap" runat="server" CssClass="panelwrapper eventcalendaredit">
 			<portal:HeadingControl ID="heading" runat="server" />
+
 			<portal:OuterBodyPanel ID="pnlOuterBody" runat="server">
 				<portal:InnerBodyPanel ID="pnlInnerBody" runat="server" CssClass="modulecontent">
-
 					<asp:Panel ID="pnlEdit" runat="server" DefaultButton="btnUpdate">
+
 						<div class="settingrow">
-							<mp:SiteLabel ID="lblTitle" runat="server" ForControl="txtTitle" CssClass="settinglabel" ConfigKey="EventCalendarEditTitleLabel" ResourceFile="EventCalResources"></mp:SiteLabel>
-							<asp:TextBox ID="txtTitle" runat="server" Columns="50" MaxLength="100" CssClass="forminput widetextbox"></asp:TextBox>
+							<mp:SiteLabel runat="server"
+								ID="lblTitle"
+								ForControl="txtTitle"
+								CssClass="settinglabel"
+								ConfigKey="EventCalendarEditTitleLabel"
+								ResourceFile="EventCalResources" />
+
+							<asp:TextBox runat="server"
+								ID="txtTitle"
+								Columns="50"
+								MaxLength="100"
+								CssClass="forminput widetextbox" />
 						</div>
+
 						<div class="settingrow">
-							<mp:SiteLabel ID="Sitelabel3" runat="server" ForControl="fckDescription" ConfigKey="EventCalendarEditDescriptionLabel" ResourceFile="EventCalResources" CssClass="settinglabel" />
+							<mp:SiteLabel runat="server"
+								ID="Sitelabel3"
+								ForControl="fckDescription"
+								ConfigKey="EventCalendarEditDescriptionLabel"
+								ResourceFile="EventCalResources"
+								CssClass="settinglabel" />
 						</div>
+
 						<div class="settingrow">
-							<mpe:EditorControl ID="edContent" runat="server"></mpe:EditorControl>
+							<mpe:EditorControl runat="server" ID="edContent" />
 						</div>
+
 						<div class="settingrow">
-							<mp:SiteLabel ID="lblStartDate" runat="server" ForControl="dpEventDate" CssClass="settinglabel" ConfigKey="EventCalendarEditEventDateLabel" ResourceFile="EventCalResources"></mp:SiteLabel>
-							<mp:DatePickerControl ID="dpEventDate" runat="server" ShowTime="False" CssClass="forminput" SkinID="eventcalendar"></mp:DatePickerControl>
+							<mp:SiteLabel runat="server"
+								ID="lblStartDate"
+								ForControl="dpEventDate" 
+								CssClass="settinglabel" 
+								ConfigKey="EventCalendarEditEventDateLabel" 
+								ResourceFile="EventCalResources" />
+
+							<mp:DatePickerControl runat="server"
+								ID="dpEventDate"
+								ShowTime="False" 
+								CssClass="forminput" 
+								SkinID="eventcalendar" />
 						</div>
+
 						<div class="settingrow">
-							<mp:SiteLabel ID="Sitelabel4" runat="server" ForControl="ddStartTime" CssClass="settinglabel" ConfigKey="EventCalendarEditStartTimeLabel" ResourceFile="EventCalResources"></mp:SiteLabel>
-							<asp:DropDownList ID="ddStartTime" runat="server" EnableTheming="false" CssClass="forminput"></asp:DropDownList>
+							<mp:SiteLabel runat="server"
+								ID="Sitelabel4" 
+								ForControl="ddStartTime" 
+								CssClass="settinglabel" 
+								ConfigKey="EventCalendarEditStartTimeLabel" 
+								ResourceFile="EventCalResources" />
+
+							<asp:DropDownList runat="server"
+								ID="ddStartTime"
+								EnableTheming="false" 
+								CssClass="forminput" />
 						</div>
+
 						<div class="settingrow">
-							<mp:SiteLabel ID="Sitelabel2" ForControl="ddEndTime" runat="server" CssClass="settinglabel" ConfigKey="EventCalendarEditEndTimeLabel" ResourceFile="EventCalResources"></mp:SiteLabel>
-							<asp:DropDownList ID="ddEndTime" runat="server" EnableTheming="false" CssClass="forminput"></asp:DropDownList>
+							<mp:SiteLabel runat="server"
+								ID="Sitelabel2" 
+								ForControl="ddEndTime" 
+								CssClass="settinglabel" 
+								ConfigKey="EventCalendarEditEndTimeLabel" 
+								ResourceFile="EventCalResources" />
+
+							<asp:DropDownList runat="server"
+								ID="ddEndTime" 
+								EnableTheming="false" 
+								CssClass="forminput" />
 						</div>
+
 						<div class="settingrow locationrow">
-							<mp:SiteLabel ID="SiteLabel1" runat="server" ForControl="txtLocation" CssClass="settinglabel" ConfigKey="LocationLabel" ResourceFile="EventCalResources"></mp:SiteLabel>
-							<asp:TextBox ID="txtLocation" runat="server" Columns="50" MaxLength="100" CssClass="forminput widetextbox"></asp:TextBox>
-							<mp:SiteLabel ID="lblShowMap" runat="server" ForControl="showMap" ConfigKey="ShowMap" ResourceFile="EventCalResources" UseLabelTag="true">
-								<asp:CheckBox ID="chkShowMap" runat="server" />
+							<mp:SiteLabel runat="server"
+								ID="SiteLabel1" 
+								ForControl="txtLocation" 
+								CssClass="settinglabel" 
+								ConfigKey="LocationLabel" 
+								ResourceFile="EventCalResources" />
+
+							<asp:TextBox runat="server"
+								ID="txtLocation" 
+								Columns="50" 
+								MaxLength="100" 
+								CssClass="forminput widetextbox" />
+
+							<mp:SiteLabel runat="server" 
+								ID="lblShowMap" 
+								ForControl="showMap" 
+								ConfigKey="ShowMap" 
+								ResourceFile="EventCalResources" 
+								UseLabelTag="true">
 							</mp:SiteLabel>
+
+							<asp:CheckBox runat="server" ID="chkShowMap" />
 						</div>
+
 						<div class="settingrow">
 							<mp:SiteLabel ID="SiteLabel35" runat="server" CssClass="settinglabel" ConfigKey="spacer" />
+
 							<div class="forminput">
-								<portal:mojoButton ID="btnUpdate" runat="server" Text="Update" />&nbsp;
-		<portal:mojoButton ID="btnDelete" runat="server" Text="Delete this item" CausesValidation="false" />&nbsp;
-		<asp:HyperLink ID="lnkCancel" runat="server" CssClass="cancellink" />&nbsp;	
-		<portal:mojoHelpLink ID="MojoHelpLink1" runat="server" HelpKey="eventedithelp" />
+								<portal:mojoButton ID="btnUpdate" runat="server" Text="Update" />
+								&nbsp;
+                        		<portal:mojoButton ID="btnDelete" runat="server" Text="Delete this item" CausesValidation="false" />
+								&nbsp;
+		                        <asp:HyperLink ID="lnkCancel" runat="server" CssClass="cancellink" />&nbsp;	
+                        		<portal:mojoHelpLink ID="MojoHelpLink1" runat="server" HelpKey="eventedithelp" />
 							</div>
 						</div>
+
 						<asp:HiddenField ID="hdnReturnUrl" runat="server" />
 					</asp:Panel>
 
@@ -55,8 +128,9 @@
 
 		</portal:InnerWrapperPanel>
 	</portal:OuterWrapperPanel>
-	<portal:SessionKeepAliveControl ID="ka1" runat="server" />
 
+	<portal:SessionKeepAliveControl ID="ka1" runat="server" />
 </asp:Content>
+
 <asp:Content ContentPlaceHolderID="rightContent" ID="MPRightPane" runat="server" />
 <asp:Content ContentPlaceHolderID="pageEditContent" ID="MPPageEdit" runat="server" />
--- mojoPortal.Features.UI/EventCalendar/EditEvent.aspx.cs
+++ mojoPortal.Features.UI/EventCalendar/EditEvent.aspx.cs
@@ -1,15 +1,3 @@
-/// Author:				        
-/// Created:			        2005-04-10
-///	Last Modified:              2013-01-15
-/// 
-/// The use and distribution terms for this software are covered by the 
-/// Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
-/// which can be found in the file CPL.TXT at the root of this distribution.
-/// By using this software in any fashion, you are agreeing to be bound by 
-/// the terms of this license.
-///
-/// You must not remove this notice, or any other, from this software.
-
 using mojoPortal.Business;
 using mojoPortal.Business.WebHelpers;
 using mojoPortal.SearchIndex;
@@ -19,298 +7,300 @@ using Resources;
 using System;
 using System.Web.UI.WebControls;
 
-namespace mojoPortal.Web.EventCalendarUI
+namespace mojoPortal.Web.EventCalendarUI;
+
+
+public partial class EventCalendarEdit : NonCmsBasePage
 {
+	private int moduleId = -1;
+	private int itemId = -1;
+	private DateTime thisDay = DateTime.Today;
 
-    public partial class EventCalendarEdit : NonCmsBasePage
+
+	#region OnInit
+
+	protected override void OnPreInit(EventArgs e)
 	{
-		private int moduleId = -1;
-        private int itemId = -1;
-        //private String cacheDependencyKey;
-        private DateTime thisDay = DateTime.Today;
-        private string virtualRoot;
-		private bool enableMap = true;
-        #region OnInit
+		AllowSkinOverride = true;
 
-        protected override void OnPreInit(EventArgs e)
-        {
-            AllowSkinOverride = true;
-            base.OnPreInit(e);
-            
-        }
+		base.OnPreInit(e);
+	}
 
-        override protected void OnInit(EventArgs e)
-        {
-            this.Load += new EventHandler(this.Page_Load);
-            this.btnUpdate.Click += new EventHandler(this.btnUpdate_Click);
-            //this.btnCancel.Click += new EventHandler(this.btnCancel_Click);
-            this.btnDelete.Click += new EventHandler(this.btnDelete_Click);
-            base.OnInit(e);
 
-            SiteUtils.SetupEditor(edContent, AllowSkinOverride, this);
-            edContent.WebEditor.ToolBar = ToolBar.Full;
-        }
+	protected override void OnInit(EventArgs e)
+	{
+		Load += new EventHandler(Page_Load);
+		btnUpdate.Click += new EventHandler(btnUpdate_Click);
+		btnDelete.Click += new EventHandler(btnDelete_Click);
 
-        #endregion
+		base.OnInit(e);
 
-        private void Page_Load(object sender, EventArgs e)
+		SiteUtils.SetupEditor(edContent, AllowSkinOverride, this);
+
+		edContent.WebEditor.ToolBar = ToolBar.Full;
+	}
+
+	#endregion
+
+
+	private void Page_Load(object sender, EventArgs e)
+	{
+		if (!Request.IsAuthenticated)
 		{
-            if (!Request.IsAuthenticated)
-            {
-                SiteUtils.RedirectToLoginPage(this);
-                return;
-            }
+			SiteUtils.RedirectToLoginPage(this);
 
-            SecurityHelper.DisableBrowserCache();
+			return;
+		}
 
-            LoadSettings();
+		SecurityHelper.DisableBrowserCache();
 
-            if (!UserCanEditModule(moduleId, CalendarEvent.FeatureGuid))
+		LoadSettings();
+
+		if (!UserCanEditModule(moduleId, CalendarEvent.FeatureGuid))
+		{
+			SiteUtils.RedirectToAccessDeniedPage(this);
+
+			return;
+		}
+
+		if (SiteUtils.IsFishyPost(this))
+		{
+			SiteUtils.RedirectToAccessDeniedPage(this);
+
+			return;
+		}
+
+		PopulateLabels();
+
+		if (!IsPostBack)
+		{
+			PopulateControls();
+
+			if (Request.UrlReferrer != null && hdnReturnUrl.Value.Length == 0)
 			{
-                SiteUtils.RedirectToAccessDeniedPage(this);
-                return;
-            }
-
-            if (SiteUtils.IsFishyPost(this))
-            {
-                SiteUtils.RedirectToAccessDeniedPage(this);
-                return;
-            }
-
-			PopulateLabels();
-
-			if (!IsPostBack) 
-			{
-				PopulateControls();
-
-                if ((Request.UrlReferrer != null) && (hdnReturnUrl.Value.Length == 0))
-                {
-                    hdnReturnUrl.Value = Request.UrlReferrer.ToString();
-                    lnkCancel.NavigateUrl = hdnReturnUrl.Value;
-                }
+				hdnReturnUrl.Value = Request.UrlReferrer.ToString();
+				lnkCancel.NavigateUrl = hdnReturnUrl.Value;
 			}
-			
 		}
+	}
 
-		private void PopulateControls()
+
+	private void PopulateControls()
+	{
+		if (itemId > -1)
 		{
-            if (itemId > -1)
-            {
-                CalendarEvent calEvent = new CalendarEvent(itemId);
+			var calEvent = new CalendarEvent(itemId);
 
-                if (calEvent.ModuleId != moduleId)
-                {
-                    SiteUtils.RedirectToAccessDeniedPage(this);
-                    return;
-                }
-
-                this.txtTitle.Text = calEvent.Title;
-                edContent.Text = calEvent.Description;
-                this.dpEventDate.Text = calEvent.EventDate.ToShortDateString();
-
-                ListItem item;
-                item = ddStartTime.Items.FindByValue(calEvent.StartTime.ToShortTimeString());
-                if (item != null)
-                {
-                    ddStartTime.ClearSelection();
-                    item.Selected = true;
-                }
-
-                item = ddEndTime.Items.FindByValue(calEvent.EndTime.ToShortTimeString());
-                if (item != null)
-                {
-                    ddEndTime.ClearSelection();
-                    item.Selected = true;
-                }
-
-                txtLocation.Text = calEvent.Location;
-
-				lblShowMap.Visible = calEvent.ShowMap;
-				chkShowMap.Visible = calEvent.ShowMap;
-				chkShowMap.Checked = calEvent.ShowMap;
-            }
-            else
-            {
-                btnDelete.Visible = false;
-            }
-			
-		}
-
-		private void PopulateLabels()
-		{
-
-            Title = SiteUtils.FormatPageTitle(siteSettings, EventCalResources.EditEventPageTitle);
-
-            heading.Text = EventCalResources.EventCalendarEditEntryLabel;
-
-            btnUpdate.Text = EventCalResources.EventCalendarEditUpdateButton;
-            SiteUtils.SetButtonAccessKey(btnUpdate, EventCalResources.EventCalendarEditUpdateButtonAccessKey);
-
-            ScriptConfig.EnableExitPromptForUnsavedContent = true;
-
-            UIHelper.DisableButtonAfterClickAndClearExitCode(
-                btnUpdate,
-                EventCalResources.ButtonDisabledPleaseWait,
-                Page.ClientScript.GetPostBackEventReference(this.btnUpdate, string.Empty)
-                );
-
-            lnkCancel.Text = EventCalResources.EventCalendarEditCancelButton;
-            //btnCancel.Text = EventCalResources.EventCalendarEditCancelButton;
-            //SiteUtils.SetButtonAccessKey(btnCancel, EventCalResources.EventCalendarEditCancelButtonAccessKey);
-
-            btnDelete.Text = EventCalResources.EventCalendarEditDeleteButton;
-            SiteUtils.SetButtonAccessKey(btnDelete, EventCalResources.EventCalendarEditDeleteButtonAccessKey);
-            UIHelper.AddConfirmationDialogWithClearExitCode(btnDelete, EventCalResources.EventCalendarDeleteEventWarning);
-
-            
-		}
-
-
-        private void btnCancel_Click(object sender, EventArgs e)
-        {
-            if (hdnReturnUrl.Value.Length > 0)
-            {
-                WebUtils.SetupRedirect(this, hdnReturnUrl.Value);
-                return;
-            }
-
-            WebUtils.SetupRedirect(this, SiteUtils.GetCurrentPageUrl());
-        }
-
-
-	    private void btnDelete_Click(object sender, EventArgs e)
-		{
-			if(itemId > -1)
+			if (calEvent.ModuleId != moduleId)
 			{
-                CalendarEvent calendarEvent = new CalendarEvent(itemId);
-                if (calendarEvent.ModuleId != moduleId)
-                {
-                    SiteUtils.RedirectToAccessDeniedPage(this);
-                    return;
-                }
+				SiteUtils.RedirectToAccessDeniedPage(this);
 
-                calendarEvent.ContentChanged += new ContentChangedEventHandler(calendarEvent_ContentChanged);
-                
-
-                calendarEvent.Delete();
-                CurrentPage.UpdateLastModifiedTime();
-                SiteUtils.QueueIndexing();
+				return;
 			}
 
-            if (hdnReturnUrl.Value.Length > 0)
-            {
-                WebUtils.SetupRedirect(this, hdnReturnUrl.Value);
-                return;
-            }
+			txtTitle.Text = calEvent.Title;
+			edContent.Text = calEvent.Description;
+			dpEventDate.Text = calEvent.EventDate.ToShortDateString();
 
-            WebUtils.SetupRedirect(this, SiteUtils.GetCurrentPageUrl());
+			var item = ddStartTime.Items.FindByValue(calEvent.StartTime.ToShortTimeString());
 
-        }
-
-        void calendarEvent_ContentChanged(object sender, ContentChangedEventArgs e)
-        {
-            IndexBuilderProvider indexBuilder = IndexBuilderManager.Providers["CalendarEventIndexBuilderProvider"];
-            if (indexBuilder != null)
-            {
-                indexBuilder.ContentChangedHandler(sender, e);
-            }
-        }
-        
-
-		private void btnUpdate_Click(object sender, EventArgs e)
-		{
-			Page.Validate("eventcalendaredit");
-			if (Page.IsValid) 
+			if (item != null)
 			{
-				int userId = -1;
-                Guid userGuid = Guid.Empty;
-				if(Request.IsAuthenticated)
+				ddStartTime.ClearSelection();
+
+				item.Selected = true;
+			}
+
+			item = ddEndTime.Items.FindByValue(calEvent.EndTime.ToShortTimeString());
+
+			if (item != null)
+			{
+				ddEndTime.ClearSelection();
+
+				item.Selected = true;
+			}
+
+			txtLocation.Text = calEvent.Location;
+			chkShowMap.Checked = calEvent.ShowMap;
+		}
+		else
+		{
+			btnDelete.Visible = false;
+		}
+	}
+
+
+	private void PopulateLabels()
+	{
+		Title = SiteUtils.FormatPageTitle(siteSettings, EventCalResources.EditEventPageTitle);
+		heading.Text = EventCalResources.EventCalendarEditEntryLabel;
+		btnUpdate.Text = EventCalResources.EventCalendarEditUpdateButton;
+
+		SiteUtils.SetButtonAccessKey(btnUpdate, EventCalResources.EventCalendarEditUpdateButtonAccessKey);
+
+		ScriptConfig.EnableExitPromptForUnsavedContent = true;
+
+		UIHelper.DisableButtonAfterClickAndClearExitCode(
+			btnUpdate,
+			EventCalResources.ButtonDisabledPleaseWait,
+			Page.ClientScript.GetPostBackEventReference(btnUpdate, string.Empty)
+		);
+
+		lnkCancel.Text = EventCalResources.EventCalendarEditCancelButton;
+		btnDelete.Text = EventCalResources.EventCalendarEditDeleteButton;
+
+		SiteUtils.SetButtonAccessKey(btnDelete, EventCalResources.EventCalendarEditDeleteButtonAccessKey);
+		UIHelper.AddConfirmationDialogWithClearExitCode(btnDelete, EventCalResources.EventCalendarDeleteEventWarning);
+	}
+
+
+	private void btnCancel_Click(object sender, EventArgs e)
+	{
+		if (hdnReturnUrl.Value.Length > 0)
+		{
+			WebUtils.SetupRedirect(this, hdnReturnUrl.Value);
+
+			return;
+		}
+
+		WebUtils.SetupRedirect(this, SiteUtils.GetCurrentPageUrl());
+	}
+
+
+	private void btnDelete_Click(object sender, EventArgs e)
+	{
+		if (itemId > -1)
+		{
+			var calendarEvent = new CalendarEvent(itemId);
+
+			if (calendarEvent.ModuleId != moduleId)
+			{
+				SiteUtils.RedirectToAccessDeniedPage(this);
+
+				return;
+			}
+
+			calendarEvent.ContentChanged += new ContentChangedEventHandler(calendarEvent_ContentChanged);
+
+
+			calendarEvent.Delete();
+			CurrentPage.UpdateLastModifiedTime();
+			SiteUtils.QueueIndexing();
+		}
+
+		if (hdnReturnUrl.Value.Length > 0)
+		{
+			WebUtils.SetupRedirect(this, hdnReturnUrl.Value);
+
+			return;
+		}
+
+		WebUtils.SetupRedirect(this, SiteUtils.GetCurrentPageUrl());
+	}
+
+
+	private void calendarEvent_ContentChanged(object sender, ContentChangedEventArgs e)
+	{
+		IndexBuilderProvider indexBuilder = IndexBuilderManager.Providers["CalendarEventIndexBuilderProvider"];
+
+		indexBuilder?.ContentChangedHandler(sender, e);
+	}
+
+
+	private void btnUpdate_Click(object sender, EventArgs e)
+	{
+		Page.Validate("eventcalendaredit");
+
+		if (Page.IsValid)
+		{
+			var userId = -1;
+			var userGuid = Guid.Empty;
+
+			if (Request.IsAuthenticated)
+			{
+				var siteUser = SiteUtils.GetCurrentSiteUser();
+
+				userId = siteUser.UserId;
+				userGuid = siteUser.UserGuid;
+			}
+
+			var calEvent = new CalendarEvent(itemId);
+
+			if (itemId > -1 && calEvent.ModuleId != moduleId)
+			{
+				SiteUtils.RedirectToAccessDeniedPage(this);
+
+				return;
+			}
+
+			calEvent.ContentChanged += new ContentChangedEventHandler(calendarEvent_ContentChanged);
+
+			var module = new Module(moduleId);
+
+			calEvent.ModuleId = module.ModuleId;
+			calEvent.ModuleGuid = module.ModuleGuid;
+			calEvent.UserId = userId;
+			calEvent.UserGuid = userGuid;
+			calEvent.LastModUserGuid = userGuid;
+			calEvent.Title = txtTitle.Text;
+			calEvent.Description = edContent.Text;
+			calEvent.EventDate = DateTime.Parse(dpEventDate.Text);
+			calEvent.StartTime = DateTime.Parse(ddStartTime.SelectedValue);
+			calEvent.EndTime = DateTime.Parse(ddEndTime.SelectedValue);
+			calEvent.Location = txtLocation.Text;
+			calEvent.ShowMap = chkShowMap.Checked;
+
+			if (calEvent.Save())
+			{
+				CurrentPage.UpdateLastModifiedTime();
+				CacheHelper.ClearModuleCache(moduleId);
+
+				SiteUtils.QueueIndexing();
+
+				if (hdnReturnUrl.Value.Length > 0)
 				{
-					//SiteUser siteUser = new SiteUser(siteSettings, Context.User.Identity.Name);
-                    SiteUser siteUser = SiteUtils.GetCurrentSiteUser();
-                    userId = siteUser.UserId;
-                    userGuid = siteUser.UserGuid;
-				}
-				CalendarEvent calEvent = new CalendarEvent(itemId);
+					WebUtils.SetupRedirect(this, hdnReturnUrl.Value);
 
-                if ((itemId > -1)&&(calEvent.ModuleId != moduleId))
-                {
-                    SiteUtils.RedirectToAccessDeniedPage(this);
-                    return;
-                }
-
-                calEvent.ContentChanged += new ContentChangedEventHandler(calendarEvent_ContentChanged);
-                
-                Module m = new Module(this.moduleId);
-                calEvent.ModuleId = m.ModuleId;
-                calEvent.ModuleGuid = m.ModuleGuid;
-				calEvent.UserId = userId;
-                calEvent.UserGuid = userGuid;
-                calEvent.LastModUserGuid = userGuid;
-				calEvent.Title = this.txtTitle.Text;
-                calEvent.Description = edContent.Text;
-				calEvent.EventDate = DateTime.Parse(this.dpEventDate.Text);
-				calEvent.StartTime = DateTime.Parse(this.ddStartTime.SelectedValue);
-				calEvent.EndTime = DateTime.Parse(this.ddEndTime.SelectedValue);
-                calEvent.Location = txtLocation.Text;
-
-               
-
-				if(calEvent.Save())
-				{
-                    CurrentPage.UpdateLastModifiedTime();
-                    //CacheHelper.TouchCacheDependencyFile(cacheDependencyKey);
-                    CacheHelper.ClearModuleCache(moduleId);
-
-                    SiteUtils.QueueIndexing();
-
-                    if (hdnReturnUrl.Value.Length > 0)
-                    {
-                        WebUtils.SetupRedirect(this, hdnReturnUrl.Value);
-                        return;
-                    }
-
-                    WebUtils.SetupRedirect(this, SiteUtils.GetCurrentPageUrl());
-         
+					return;
 				}
 
+				WebUtils.SetupRedirect(this, SiteUtils.GetCurrentPageUrl());
 			}
+		}
+	}
+
+
+	private void LoadSettings()
+	{
+		moduleId = WebUtils.ParseInt32FromQueryString("mid", -1);
+		itemId = WebUtils.ParseInt32FromQueryString("ItemID", -1);
+		thisDay = WebUtils.ParseDateFromQueryString("date", DateTime.Today);
+
+		lnkCancel.NavigateUrl = SiteUtils.GetCurrentPageUrl();
+
+		if (!Page.IsPostBack)
+		{
+			dpEventDate.Text = thisDay.ToShortDateString();
+		}
+
+		var tomorrow = thisDay.AddDays(1).AddMinutes(-15);
+
+		ddStartTime.Items.Insert(0, new ListItem(thisDay.ToShortTimeString(), thisDay.ToShortTimeString()));
+		ddEndTime.Items.Insert(0, new ListItem(thisDay.ToShortTimeString(), thisDay.ToShortTimeString()));
+
+		var i = 0;
+
+		while (thisDay < tomorrow)
+		{
+			i += 1;
+
+			thisDay = thisDay.AddMinutes(15);
+
+			ddStartTime.Items.Insert(i, new ListItem(thisDay.ToShortTimeString(), thisDay.ToShortTimeString()));
+			ddEndTime.Items.Insert(i, new ListItem(thisDay.ToShortTimeString(), thisDay.ToShortTimeString()));
 
 		}
 
-
-        private void LoadSettings()
-        {
-            virtualRoot = WebUtils.GetApplicationRoot();
-            moduleId = WebUtils.ParseInt32FromQueryString("mid", -1);
-            itemId = WebUtils.ParseInt32FromQueryString("ItemID", -1);
-            thisDay = WebUtils.ParseDateFromQueryString("date", DateTime.Today);
-            //cacheDependencyKey = "Module-" + moduleId.ToString();
-            
-            lnkCancel.NavigateUrl = SiteUtils.GetCurrentPageUrl();
-            
-            if (!Page.IsPostBack)
-            {
-                dpEventDate.Text = thisDay.ToShortDateString();
-            }
-
-            DateTime tomorrow = thisDay.AddDays(1).AddMinutes(-15);
-            ddStartTime.Items.Insert(0, new ListItem(thisDay.ToShortTimeString(), thisDay.ToShortTimeString()));
-            ddEndTime.Items.Insert(0, new ListItem(thisDay.ToShortTimeString(), thisDay.ToShortTimeString()));
-            int i = 0;
-            while (thisDay < tomorrow)
-            {
-                i += 1;
-                thisDay = thisDay.AddMinutes(15);
-
-                ddStartTime.Items.Insert(i, new ListItem(thisDay.ToShortTimeString(), thisDay.ToShortTimeString()));
-                ddEndTime.Items.Insert(i, new ListItem(thisDay.ToShortTimeString(), thisDay.ToShortTimeString()));
-
-            }
-
-            AddClassToBody("eventcaledit");
-        }
-
-		
+		AddClassToBody("eventcaledit");
 	}
 }
