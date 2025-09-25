63356d13e0705de1c330421f470f5f299ae65209
Joe Davis <joe@i7media.net>
2025-09-25 10:38:21 -0500

    move styleset.cs back to web

--- Web/Components/StyleSet.cs
+++ Web/Components/StyleSet.cs
@@ -1,8 +1,9 @@
-﻿namespace mojoPortal.Business;
+﻿namespace mojoPortal.Web;
 
 public class StyleSet
 {
 	public string Name { get; set; }
 	public string CssClasses { get; set; }
 	public string Description { get; set; }
+	public string ApplyTo { get; set; } // Feature Strong Name
 }
