912b1030516ab1c5b85d93d450820d35f97ce90e
Elijah Fowler <j.elijah.fowler@gmail.com>
2025-09-26 15:38:50 -0500

    Migrated search controls to razor

--- Web/Models/SearchResultsViewModel.cs
+++ Web/Models/SearchResultsViewModel.cs
@@ -1,68 +1,156 @@
 ﻿#nullable enable
 using mojoPortal.SearchIndex;
 using mojoPortal.Web.UI;
+using Resources;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using static System.Net.Mime.MediaTypeNames;
+using System.Web.Mvc;
 
 namespace mojoPortal.Web.Models;
 
 public record SearchResultsViewModel(
+	string PageTitle,
+	Controls Controls,
+	ResultsMessage ResultsMessage,
 	Results Results,
 	PagerInfo Pagination,
-	List<Item> Items
+	HelpLink HelpLink,
+	Filters Filters
 )
 {
 	public static SearchResultsViewModel Create(
+		string pageTitle,
 		int pageNumber,
 		int pageSize,
 		int totalItemCount,
-		bool showModuleTitle,
-		string pagerUrlFormat,
+		bool showModuleTitleInResultLink,
+		string pageUrlFormat,
 		string queryText,
-		TimeZoneInfo timeZone,
-		IndexItemCollection items,
-		SearchResultsDisplaySettings displaySettings
+		TimeZoneInfo? timeZone,
+		IndexItemCollection? items,
+		SearchResultsDisplaySettings displaySettings,
+		float? duration,
+		List<SelectListItem> featureList,
+		DateTime dateStart,
+		DateTime dateEnd,
+		bool queryErrorOccurred
 	)
 	{
 		var from = ((pageNumber - 1) * pageSize) + 1;
 		var to = Math.Min(from + pageSize - 1, totalItemCount);
 
 		return new(
+			pageTitle,
+			new Controls(
+				new SearchInput(queryText),
+				new SearchButton(Resource.SearchButtonText)
+			),
+			new ResultsMessage(
+				Preamble: Resource.SearchResultsLabel,
+				From: from,
+				ToText: Resource.SearchResultsOfLabel,
+				To: to,
+				ForText: Resource.SearchResultsForLabel,
+				Total: totalItemCount,
+				Query: queryText,
+				Duration: duration,
+				DurationText: Resource.SearchResultsSecondsLabel
+			),
 			new Results(
-				from,
-				to,
-				totalItemCount,
-				HttpUtility.HtmlEncode(queryText)
+				Items: [.. items?
+					.Cast<IndexItem>()
+					.Select(x => new Item(
+						x.ItemId,
+						new Link(x, showModuleTitleInResultLink),
+						new Content(x.Intro),
+						new Author(x.Author, displaySettings.ShowAuthor),
+						new Date(x.CreatedUtc, timeZone, displaySettings.ShowCreatedDate),
+						new Date(x.LastModUtc, timeZone, displaySettings.ShowLastModDate),
+						!string.IsNullOrWhiteSpace(x.ItemImage) ? new Image(x.ItemImage, x.Title) : null,
+						!string.IsNullOrWhiteSpace(x.Sku) ? x.Sku : null,
+						!string.IsNullOrWhiteSpace(x.FeatureId) ? x.FeatureId : null,
+						!string.IsNullOrWhiteSpace(x.FeatureResourceFile) && !string.IsNullOrWhiteSpace(x.FeatureName) ?
+							HttpContext.GetGlobalResourceObject(x.FeatureResourceFile, x.FeatureName) as string :
+							null
+					)) ?? []],
+				ShowResults: items is not null && totalItemCount > 0,
+				ShowNoResults: queryErrorOccurred || !string.IsNullOrWhiteSpace(queryText) && totalItemCount == 0,
+				NoResultsText: queryErrorOccurred ? Resource.SearchQueryInvalid : Resource.SearchResultsNotFound
 			),
 			new PagerInfo(
 				pageNumber,
 				pageSize,
 				(int)Math.Ceiling((double)totalItemCount / pageSize),
-				pagerUrlFormat
+				pageUrlFormat
 			),
-			[.. items
-				.Cast<IndexItem>()
-				.Select(x => new Item(
-					new Link(x, showModuleTitle),
-					new Content(x.Intro),
-					new Author(x.Author, displaySettings.ShowAuthor),
-					new Date(x.CreatedUtc, timeZone, displaySettings.ShowCreatedDate),
-					new Date(x.LastModUtc, timeZone, displaySettings.ShowLastModDate),
-					!string.IsNullOrWhiteSpace(x.ItemImage) ? new Image(x.ItemImage, x.Title) : null
-				))]
+			new HelpLink(
+				Resource.HelpLink,
+				"Help.aspx".ToLinkBuilder().AddParam("helpkey", "search-help").ToString(),
+				Resource.CloseDialogButton
+			),
+			new Filters(
+				new Feature(
+					featureList,
+					!WebConfigSettings.DisableSearchFeatureFilters && displaySettings.ShowFeatureFilter && featureList.Count > 1
+				),
+				new DateRange(
+					Resource.SearchDateFilterPreamble,
+					dateStart.Date > DateTime.MinValue.Date ? dateStart.ToShortDateString() : string.Empty,
+					Resource.and,
+					dateEnd.Date < DateTime.MaxValue.Date ? dateEnd.ToShortDateString() : string.Empty,
+					!WebConfigSettings.DisableSearchFeatureFilters && displaySettings.ShowModifiedDateFilters
+				)
+			)
 		);
 	}
 }
 
 
-public record Results(
+public record SearchInput(string Value);
+public record SearchButton(string Text);
+public record Controls(SearchInput SearchInput, SearchButton SearchButton);
+
+
+public record Filters(
+	Feature Features,
+	DateRange DateRange
+);
+
+
+public record Feature(
+	List<SelectListItem> Options,
+	bool Show
+);
+
+
+public record DateRange(
+	string Preamble,
+	string DateStart,
+	string AndText,
+	string DateEnd,
+	bool Show
+);
+
+
+public record ResultsMessage(
+	string Preamble,
 	int From,
+	string ToText,
 	int To,
+	string ForText,
 	int Total,
-	string Query
+	string Query,
+	float? Duration,
+	string DurationText
+);
+
+public record Results(
+	List<Item> Items,
+	bool ShowResults,
+	bool ShowNoResults,
+	string NoResultsText
 );
 
 
@@ -137,6 +225,13 @@ public record Link
 }
 
 
+public record HelpLink(
+	string Text,
+	string Href,
+	string CloseButtonText
+);
+
+
 public record Content(string Text)
 {
 	public override string ToString()
@@ -198,10 +293,14 @@ public record Image(string Href, string Alt);
 
 
 public record Item(
+	int? ItemId,
 	Link Link,
 	Content Content,
 	Author Author,
 	Date CreatedDate,
 	Date ModifiedDate,
-	Image? Image = null
+	Image? Image = null,
+	string? Sku = null,
+	string? FeatureId = null,
+	string? FeatureName = null
 );
