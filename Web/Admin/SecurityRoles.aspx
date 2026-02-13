7259b97213fc207af57f8ef5e2c88dbeca844070
Elijah Fowler <j.elijah.fowler@gmail.com>
2026-02-12 19:19:01 -0600

    Change SecurityRoles page to require user to enter password assigning roles to users

--- Web/Admin/SecurityRoles.aspx
+++ Web/Admin/SecurityRoles.aspx
@@ -1,56 +1,72 @@
-<%@ Page language="c#" CodeBehind="SecurityRoles.aspx.cs" MasterPageFile="~/App_MasterPages/layout.Master" AutoEventWireup="false" Inherits="mojoPortal.Web.AdminUI.SecurityRoles" %>
+<%@ Page Language="c#" CodeBehind="SecurityRoles.aspx.cs" MasterPageFile="~/App_MasterPages/layout.Master" AutoEventWireup="false" Inherits="mojoPortal.Web.AdminUI.SecurityRoles" %>
 
 <asp:Content ContentPlaceHolderID="leftContent" ID="MPLeftPane" runat="server" />
+
 <asp:Content ContentPlaceHolderID="mainContent" ID="MPContent" runat="server">
-<portal:AdminCrumbContainer ID="pnlAdminCrumbs" runat="server" CssClass="breadcrumbs">
-<asp:HyperLink ID="lnkAdminMenu" runat="server" NavigateUrl="~/Admin/AdminMenu.aspx" /><portal:AdminCrumbSeparator id="AdminCrumbSeparator1" runat="server" Text="&nbsp;&gt;" EnableViewState="false" />
-<asp:HyperLink ID="lnkRoleManager" runat="server" NavigateUrl="~/Admin/RoleManager.aspx" />
-</portal:AdminCrumbContainer>
-<portal:OuterWrapperPanel ID="pnlOuterWrap" runat="server">
+	<portal:AdminCrumbContainer ID="pnlAdminCrumbs" runat="server" CssClass="breadcrumbs">
+		<asp:HyperLink ID="lnkAdminMenu" runat="server" NavigateUrl="~/Admin/AdminMenu.aspx" />
+		<portal:AdminCrumbSeparator ID="AdminCrumbSeparator1" runat="server" Text="&nbsp;&gt;" EnableViewState="false" />
+		<asp:HyperLink ID="lnkRoleManager" runat="server" NavigateUrl="~/Admin/RoleManager.aspx" />
+	</portal:AdminCrumbContainer>
 
-<portal:InnerWrapperPanel ID="pnlInnerWrap" runat="server" CssClass="panelwrapper admin securityroles ">
-<portal:HeadingControl id="heading" runat="server" />
-<portal:OuterBodyPanel ID="pnlOuterBody" runat="server">
-<portal:InnerBodyPanel ID="pnlInnerBody" runat="server" CssClass="modulecontent">
-<asp:Panel id="pnlSecurity" runat="server"  CssClass="securityroles">
-    <div class="settingrow">
-        <asp:HyperLink id="lnkFindUser" runat="server" CssClass="cblink" />
-        <asp:HiddenField ID="hdnUserID" runat="server" />
-        <asp:ImageButton ID="btnSetUserFromGreyBox" runat="server" />
-    </div>
-    <div class="settingrow">
-        <asp:Repeater ID="rptRoleMembers" runat="server">
-            <HeaderTemplate>
-                <ul class="simplelist">
-            </HeaderTemplate>
-            <ItemTemplate>
-                <li class="simplelist">
-                    <asp:ImageButton ImageUrl='<%# DeleteLinkImage %>' 
-                    AlternateText='<%# Resources.Resource.ManageUsersRemoveFromRoleButton %>'
-                     ToolTip='<%# Resources.Resource.ManageUsersRemoveFromRoleButton %>'
-                    CommandName="delete" 
-                    CommandArgument='<%# DataBinder.Eval(Container.DataItem, "UserID") %>' runat="server" ID="btnDelete"  />
-				<asp:Label Text='<%# DataBinder.Eval(Container.DataItem, "Name") %>' runat="server" ID="Label1" EnableViewState="false" />
-                <asp:HyperLink ID="lnkManageUser" runat="server" EnableViewState="false" Visible='<%# CanManageUsers %>'
-                    NavigateUrl='<%# SiteRoot + "/Admin/ManageUsers.aspx?userid=" + Eval("UserID") %>' Text='<%# Resources.Resource.ManageUsersPageTitle %>' />
-                </li>
-            </ItemTemplate>
-            <FooterTemplate>
-                </ul>
-            </FooterTemplate>
-        </asp:Repeater>
-    </div>
-    <div class="settingrow">
-        <portal:mojoLabel id="Message" runat="server" CssClass="txterror warning" />
-    </div>
-    <portal:mojoCutePager ID="pgr" runat="server" />
-</asp:Panel>
-</portal:InnerBodyPanel>
-</portal:OuterBodyPanel>
+	<portal:OuterWrapperPanel ID="pnlOuterWrap" runat="server">
 
-</portal:InnerWrapperPanel>
+		<portal:InnerWrapperPanel ID="pnlInnerWrap" runat="server" CssClass="panelwrapper admin securityroles ">
+			<portal:HeadingControl ID="heading" runat="server" />
 
-</portal:OuterWrapperPanel>
+			<portal:OuterBodyPanel ID="pnlOuterBody" runat="server">
+				<portal:InnerBodyPanel ID="pnlInnerBody" runat="server" CssClass="modulecontent">
+					<asp:Panel ID="pnlSecurity" runat="server" CssClass="securityroles">
+						<div class="settingrow">
+							<asp:HyperLink runat="server"
+								ID="lnkFindUser"
+								CssClass="add-user-to-role-link"
+								data-modal=""
+								data-size="fluid-xlarge"
+								data-close-text="Close"
+								data-modal-type="iframe"
+								data-height="full" />
+							<asp:HiddenField ID="hdnUserPassword" runat="server" />
+							<asp:HiddenField ID="hdnUserID" runat="server" />
+							<asp:ImageButton ID="btnSetUserFromGreyBox" runat="server" />
+						</div>
+
+						<div class="settingrow">
+							<asp:Repeater ID="rptRoleMembers" runat="server">
+								<HeaderTemplate>
+									<ul class="simplelist">
+								</HeaderTemplate>
+
+								<ItemTemplate>
+									<li class="simplelist">
+										<asp:ImageButton ImageUrl='<%# DeleteLinkImage %>'
+											AlternateText='<%# Resources.Resource.ManageUsersRemoveFromRoleButton %>'
+											ToolTip='<%# Resources.Resource.ManageUsersRemoveFromRoleButton %>'
+											CommandName="delete"
+											CommandArgument='<%# DataBinder.Eval(Container.DataItem, "UserID") %>' runat="server" ID="btnDelete" />
+										<asp:Label Text='<%# DataBinder.Eval(Container.DataItem, "Name") %>' runat="server" ID="Label1" EnableViewState="false" />
+										<asp:HyperLink ID="lnkManageUser" runat="server" EnableViewState="false" Visible='<%# CanManageUsers %>'
+											NavigateUrl='<%# SiteRoot + "/Admin/ManageUsers.aspx?userid=" + Eval("UserID") %>' Text='<%# Resources.Resource.ManageUsersPageTitle %>' />
+									</li>
+								</ItemTemplate>
+
+								<FooterTemplate>
+									</ul>
+								</FooterTemplate>
+							</asp:Repeater>
+						</div>
+
+						<div class="settingrow">
+							<portal:mojoLabel ID="Message" runat="server" CssClass="txterror warning" />
+						</div>
+
+						<portal:mojoCutePager ID="pgr" runat="server" />
+					</asp:Panel>
+				</portal:InnerBodyPanel>
+			</portal:OuterBodyPanel>
+		</portal:InnerWrapperPanel>
+	</portal:OuterWrapperPanel>
 </asp:Content>
+
 <asp:Content ContentPlaceHolderID="rightContent" ID="MPRightPane" runat="server" />
 <asp:Content ContentPlaceHolderID="pageEditContent" ID="MPPageEdit" runat="server" />
--- Web/Admin/SecurityRoles.aspx.cs
+++ Web/Admin/SecurityRoles.aspx.cs
@@ -1,27 +1,49 @@
-using System;
-using System.Data;
-using System.Text;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 using mojoPortal.Business;
 using mojoPortal.Business.WebHelpers;
-using mojoPortal.Core.Extensions;
 using mojoPortal.Web.Framework;
 using Resources;
+using System;
+using System.Data;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
 
 namespace mojoPortal.Web.AdminUI;
 
 public partial class SecurityRoles : NonCmsBasePage
 {
-	private int roleID = -1;
-	private bool isAdmin = false;
+	#region Fields
+
+	private bool _isAdmin = false;
+	private int _roleId = -1;
+	private Role _role;
+	private int _totalPages = 1;
+	private int _pageNumber = 1;
+	private int _pageSize = 20;
+
 	protected bool CanManageUsers = false;
-	private Role role;
 	protected string DeleteLinkImage = "~/Data/SiteImages/" + WebConfigSettings.DeleteLinkImage;
 
-	private int totalPages = 1;
-	private int pageNumber = 1;
-	private int pageSize = 20;
+	#endregion
+
+
+	#region Overrides
+
+	protected override void OnInit(EventArgs e)
+	{
+		base.OnInit(e);
+
+		Load += new EventHandler(Page_Load);
+		btnSetUserFromGreyBox.Click += new ImageClickEventHandler(BtnSetUserFromGreyBox_Click);
+		rptRoleMembers.ItemCommand += new RepeaterCommandEventHandler(RptRoleMembers_ItemCommand);
+		rptRoleMembers.ItemDataBound += new RepeaterItemEventHandler(RptRoleMembers_ItemDataBound);
+
+		SuppressMenuSelection();
+		SuppressPageMenu();
+	}
+
+	#endregion
+
 
 	private void Page_Load(object sender, EventArgs e)
 	{
@@ -46,7 +68,7 @@ public partial class SecurityRoles : NonCmsBasePage
 		SecurityHelper.DisableBrowserCache();
 
 		LoadParams();
-		role = new Role(roleID);
+		_role = new Role(_roleId);
 		EnforceSecurity();
 		SetupScript();
 		PopulateLabels();
@@ -54,110 +76,131 @@ public partial class SecurityRoles : NonCmsBasePage
 		if (!Page.IsPostBack)
 		{
 			BindData();
-
 		}
 	}
 
-	void btnSetUserFromGreyBox_Click(object sender, System.Web.UI.ImageClickEventArgs e)
+
+	#region Event Methods
+
+	private void BtnSetUserFromGreyBox_Click(object sender, ImageClickEventArgs e)
 	{
-		if (hdnUserID.Value.Length == 0) { return; }
+		if (
+			string.IsNullOrWhiteSpace(hdnUserID.Value) ||
+			string.IsNullOrWhiteSpace(hdnUserPassword.Value))
+		{
+			return;
+		}
+
 		try
 		{
-			int userId = Convert.ToInt32(hdnUserID.Value);
-			SiteUser user = new SiteUser(siteSettings, userId);
+			var userId = Convert.ToInt32(hdnUserID.Value);
+			var user = new SiteUser(siteSettings, userId);
+			var membershipProvider = Membership.Provider as mojoMembershipProvider;
+			var isValidRequest = membershipProvider.ValidateUser(User.Identity.Name, hdnUserPassword.Value);
 
-			Role.AddUser(roleID, userId, role.RoleGuid, user.UserGuid);
+			if (!isValidRequest)
+			{
+				// Provided password did not match logged in user, do not change roles
+				WebUtils.SetupRedirect(this, Request.RawUrl);
+				return;
+			}
+
+			Role.AddUser(_roleId, userId, _role.RoleGuid, user.UserGuid);
+
 			user.RolesChanged = true;
+
 			user.Save();
 
 			WebUtils.SetupRedirect(this, Request.RawUrl);
 
 		}
 		catch (FormatException) { }
-
 	}
 
-	void rptRoleMembers_ItemCommand(object source, RepeaterCommandEventArgs e)
+
+	private void RptRoleMembers_ItemCommand(object source, RepeaterCommandEventArgs e)
 	{
 		if (e.CommandName == "delete")
 		{
-			int userId = Convert.ToInt32(e.CommandArgument);
+			var userId = Convert.ToInt32(e.CommandArgument);
+			var user = new SiteUser(siteSettings, userId);
 
-			SiteUser user = new SiteUser(siteSettings, userId);
-
-			Role.RemoveUser(roleID, userId);
+			Role.RemoveUser(_roleId, userId);
 
 			if (user.UserId > -1)
 			{
 				user.RolesChanged = true;
 				user.Save();
 			}
-
-
 		}
 
 		WebUtils.SetupRedirect(this, Request.RawUrl);
-
 	}
 
 
-	void rptRoleMembers_ItemDataBound(object sender, RepeaterItemEventArgs e)
+	private void RptRoleMembers_ItemDataBound(object sender, RepeaterItemEventArgs e)
 	{
-		ImageButton btnDelete = e.Item.FindControl("btnDelete") as ImageButton;
-		if (btnDelete != null)
+		if (e.Item.FindControl("btnDelete") is ImageButton btnDelete)
 		{
 			btnDelete.AlternateText = Resource.ManageUsersRemoveFromRoleButton;
 			btnDelete.ToolTip = Resource.ManageUsersRemoveFromRoleButton;
+
 			UIHelper.AddConfirmationDialog(btnDelete, Resource.RolesRemoveUserWarning);
 		}
-
 	}
 
+	#endregion
+
+
+	#region Private Methods
 
 	private void BindData()
 	{
+		heading.Text = $"{Resource.SecurityRolesTitle} {_role.DisplayName}";
 
-		heading.Text = Resource.SecurityRolesTitle + " " + role.DisplayName;
-
-		using (IDataReader reader = Role.GetUsersInRole(
+		using IDataReader reader = Role.GetUsersInRole(
 			siteSettings.SiteId,
-			roleID,
-			pageNumber,
-			pageSize,
-			out totalPages))
-		{
-			string pageUrl = SiteRoot + "/Admin/SecurityRoles.aspx?roleid=" + roleID.ToInvariantString() + "&amp;pagenumber={0}";
+			_roleId,
+			_pageNumber,
+			_pageSize,
+			out _totalPages);
 
-			pgr.PageURLFormat = pageUrl;
-			pgr.ShowFirstLast = true;
-			pgr.CurrentIndex = pageNumber;
-			pgr.PageSize = pageSize;
-			pgr.PageCount = totalPages;
-			pgr.Visible = (totalPages > 1);
+		var pageUrl = "~/Admin/SecurityRoles.aspx"
+			.ToLinkBuilder()
+			.AddParam("roleid", _roleId)
+			.AddParam("pagenumber", "{0}")
+			.ToString();
 
-			rptRoleMembers.DataSource = reader;
-			rptRoleMembers.DataBind();
-		}
+		pgr.PageURLFormat = pageUrl;
+		pgr.ShowFirstLast = true;
+		pgr.CurrentIndex = _pageNumber;
+		pgr.PageSize = _pageSize;
+		pgr.PageCount = _totalPages;
+		pgr.Visible = _totalPages > 1;
 
+		rptRoleMembers.DataSource = reader;
 
+		rptRoleMembers.DataBind();
 	}
+
 
 	private void EnforceSecurity()
 	{
-		if (role.RoleId == -1)
-		{
-			pnlSecurity.Visible = false;
-			return;
-		}
-		if (role.SiteId != siteSettings.SiteId)
+		if (_role.RoleId == -1)
 		{
 			pnlSecurity.Visible = false;
 			return;
 		}
 
-		if (!isAdmin)
+		if (_role.SiteId != siteSettings.SiteId)
 		{
-			if ((role.Equals("Admins")) || (role.Equals("Role Admins")))
+			pnlSecurity.Visible = false;
+			return;
+		}
+
+		if (!_isAdmin)
+		{
+			if (_role.Equals("Admins") || _role.Equals("Role Admins"))
 			{
 				pnlSecurity.Visible = false;
 				return;
@@ -165,7 +208,7 @@ public partial class SecurityRoles : NonCmsBasePage
 
 			if (!WebConfigSettings.AllowRoleAdminsToCreateContentManagers)
 			{
-				if (role.Equals("Content Administrators"))
+				if (_role.Equals("Content Administrators"))
 				{
 					pnlSecurity.Visible = false;
 					return;
@@ -181,60 +224,55 @@ public partial class SecurityRoles : NonCmsBasePage
 
 		lnkAdminMenu.Text = Resource.AdminMenuLink;
 		lnkAdminMenu.ToolTip = Resource.AdminMenuLink;
-		lnkAdminMenu.NavigateUrl = SiteRoot + "/Admin/AdminMenu.aspx";
+		lnkAdminMenu.NavigateUrl = "~/Admin/AdminMenu.aspx".ToLinkBuilder().ToString();
 		lnkRoleManager.Text = Resource.AdminMenuRoleAdminLink;
 		lnkRoleManager.ToolTip = Resource.AdminMenuRoleAdminLink;
-		lnkRoleManager.NavigateUrl = SiteRoot + "/Admin/RoleManager.aspx";
-
-		//lnkUserLookup.Text = Resource.SecurityAddExistingButton;
-		//lnkUserLookup.ToolTip = Resource.SecurityAddExistingButton;
-		//lnkUserLookup.DialogCloseText = Resource.CloseDialogButton;
-		//lnkUserLookup.NavigateUrl = SiteRoot + "/Dialog/RoleUserSelectDialog.aspx?r=" + roleID.ToInvariantString();
+		lnkRoleManager.NavigateUrl = "~/Admin/RoleManager.aspx".ToLinkBuilder().ToString();
 
 		lnkFindUser.Text = Resource.SecurityAddExistingButton;
 		lnkFindUser.ToolTip = Resource.SecurityAddExistingButton;
-		lnkFindUser.NavigateUrl = SiteRoot + "/Dialog/RoleUserSelectDialog.aspx?r=" + roleID.ToInvariantString();
+		lnkFindUser.NavigateUrl = "~/Dialog/RoleUserSelectDialog.aspx"
+			.ToLinkBuilder()
+			.AddParam("r", _roleId)
+			.ToString();
 
-
-
-
-		btnSetUserFromGreyBox.ImageUrl = Page.ResolveUrl("~/Data/SiteImages/1x1.gif");
+		btnSetUserFromGreyBox.ImageUrl = "~/Data/SiteImages/1x1.gif".ToLinkBuilder().ToString();
 		btnSetUserFromGreyBox.Attributes.Add("tabIndex", "-1");
 		btnSetUserFromGreyBox.AlternateText = " ";
 	}
 
+
 	private void SetupScript()
 	{
-		StringBuilder script = new StringBuilder();
+		var script = $$"""
 
-		script.Append("\n<script type='text/javascript'>");
-		script.Append("function SelectUser(userId) {");
+			<script>
+				function SelectUser(userId, password) {
+					const hdnUI = document.getElementById('{{hdnUserID.ClientID}}');
+					const hdnUserPassword = document.getElementById('{{hdnUserPassword.ClientID}}');
 
-		//script.Append("GB_hide();");
+					hdnUI.value = userId;
+					hdnUserPassword.value = password;
 
-		script.Append("var hdnUI = document.getElementById('" + this.hdnUserID.ClientID + "'); ");
-		script.Append("hdnUI.value = userId; ");
+					const btn = document.getElementById('{{btnSetUserFromGreyBox.ClientID}}');
 
-		script.Append("var btn = document.getElementById('" + this.btnSetUserFromGreyBox.ClientID + "');  ");
-		script.Append("btn.click(); ");
+					btn.click();
 
-		script.Append("$.colorbox.close(); ");
-
-		script.Append("}");
-		script.Append("</script>");
-
+					$('.modal').modal('hide');
+				}
+			</script>
+			""";
 
 		Page.ClientScript.RegisterStartupScript(typeof(Page), "SelectUserHandler", script.ToString());
-
 	}
 
 
 	private void LoadParams()
 	{
+		_roleId = WebUtils.ParseInt32FromQueryString("roleid", -1);
+		_isAdmin = WebUser.IsAdmin;
 
-		roleID = WebUtils.ParseInt32FromQueryString("roleid", -1);
-		isAdmin = WebUser.IsAdmin;
-		if (isAdmin)
+		if (_isAdmin)
 		{
 			CanManageUsers = true;
 		}
@@ -245,30 +283,12 @@ public partial class SecurityRoles : NonCmsBasePage
 
 		ScriptConfig.IncludeColorBox = true;
 
-		pageSize = WebConfigSettings.RoleMemberPageSize;
-		pageNumber = WebUtils.ParseInt32FromQueryString("pagenumber", pageNumber);
+		_pageSize = WebConfigSettings.RoleMemberPageSize;
+		_pageNumber = WebUtils.ParseInt32FromQueryString("pagenumber", _pageNumber);
 
 		AddClassToBody("administration");
 		AddClassToBody("securityroles");
-
-
 	}
 
-
-	override protected void OnInit(EventArgs e)
-	{
-		base.OnInit(e);
-		this.Load += new EventHandler(this.Page_Load);
-
-
-		btnSetUserFromGreyBox.Click += new System.Web.UI.ImageClickEventHandler(btnSetUserFromGreyBox_Click);
-		rptRoleMembers.ItemCommand += new RepeaterCommandEventHandler(rptRoleMembers_ItemCommand);
-		rptRoleMembers.ItemDataBound += new RepeaterItemEventHandler(rptRoleMembers_ItemDataBound);
-
-
-		SuppressMenuSelection();
-		SuppressPageMenu();
-	}
-
-
+	#endregion
 }
